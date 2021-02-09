d0de9b1504d11dcb6ff1d57e6cd18792750372bb
Mylo James <mjames@appacademy.io>
2021-02-09 08:26:03 -0600

    changed firstFor

--- week-1_lectures/d2-functions/snippets/firstFor.js
+++ week-1_lectures/d2-functions/snippets/firstFor.js
@@ -2,7 +2,7 @@
 Instructions:
 Write a function called firstFor
 that uses a for loop to print 
-all numbers descending from 7 and 2.
+all numbers descending from 7 to 2.
 
 Then call your function
 */
