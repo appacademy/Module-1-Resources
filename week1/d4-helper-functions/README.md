1a59080ac54e6888c16071bbc4ea660af88516ae
Mylo James <mjames@appacademy.io>
2021-03-10 19:05:32 -0600

    made readme for helperfunctions

--- week1/d4-helper-functions/README.md
+++ week1/d4-helper-functions/README.md
@@ -1,1 +1,13 @@
-* There are no slides for this day, please reference helperFunction.js file
+# W1D4 - Helper Functions
+
+## [Morning Boost]
+
+## Helper Functions
+
+`Video Lectures`
+
+- [Helper Functions Demo]
+  - [Video Code](./code-it-out/helperFunction.js)
+
+[Helper Functions Demo]: https://open.appacademy.io/learn/js-py---mar-2021-cohort-1-online/week-1-mar-2021-cohort-1-online/helper-functions-demo
+[Morning Boost]: https://open.appacademy.io/learn/js-py---mar-2021-cohort-1-online/week-1-mar-2021-cohort-1-online/thursday-morning-boost
--- week1/d4-helper-functions/code-it-out/helperFunction.js
+++ week1/d4-helper-functions/code-it-out/helperFunction.js
@@ -1,38 +1,11 @@
-// Here, we are using a function we previosuly defined to help with the logic of another function.
-// This is a VERY common pattern and will become more and more relevant as we progress in the curriculum.
-// NOTE:
-  // When we define a function, that does not execute the logic we wrote.
-  // The logic inside the functin will run when we invoke the function.
-
-
-
-// we're going to be solving a problem that extracts even numbers from an array
-
-
-
-
-
-
-
-
-
-debugger
 let isEven = function (num) {
-  debugger
   return num % 2 === 0;
 };
-
-debugger
-isEven
-
+/* 
+In your own words, what does the isEven function do?
 
 
-
-
-
-
-
-
+*/
 let extractEvens = function (numbers) {
   let evens = [];
 
@@ -43,34 +16,17 @@ let extractEvens = function (numbers) {
       evens.push(num);
     }
   }
-
   return evens;
 };
 
 let evensArr = [3, 5, 4, 7, 8];
 console.log(extractEvens(evensArr));
+/* 
+Where is the helper function?
 
+Why would a developer decide to use a helper function?
 
-
-
-
-
-
-
-
-
-
-
-
-
-// we're going to solving a problem that grabs all primes from an array
-
-
-
-
-
-
-
+*/
 
 let isPrime = function (num) {
   if (num < 2) {
@@ -86,6 +42,12 @@ let isPrime = function (num) {
   return true;
 }
 
+/* 
+In your own words, what is this function doing?
+
+
+*/
+
 let pickPrimes = function (nums) {
   let primes = [];
 
@@ -100,5 +62,11 @@ let pickPrimes = function (nums) {
   return primes;
 }
 
-// console.log(pickPrimes([4, 7, 5, 12]));
-// console.log(pickPrimes([31, 25, 9, 16]));
+console.log(pickPrimes([4, 7, 5, 12]));
+console.log(pickPrimes([31, 25, 9, 16]));
+
+/* 
+Where is the helper function?
+
+
+*/
