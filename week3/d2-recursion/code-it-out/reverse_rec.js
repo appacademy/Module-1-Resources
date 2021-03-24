2c3028999d5ed5418ec5032d120e91b4ffac7bc1
Mylo James <mjames@appacademy.io>
2021-03-24 09:30:27 -0500

    w3d3

--- week3/d2-recursion/code-it-out/reverse_rec.js
+++ week3/d2-recursion/code-it-out/reverse_rec.js
@@ -1,14 +1,73 @@
-function reverse_rec(string) {
+'esuoh'
+
+function reverse(string) {
     if (string.length === 0) {
-        // base case - string.length === 0;
         return '';
     }
 
-    let sub_str = reverse_rec(string.slice(0, string.length - 1));
-    // recursive case - while string.length > 0
-    // recursive step - string.slice(0, string.length - 1)
+    let sub_str = 'suoh'
+
+    return 'e' + 'suoh'
+}
+
+console.log(reverse('house'));
+/*******************************/
+function reverse(string) {
+    if (string.length === 0) {
+        return '';
+    }
+
+    let sub_str = 'uoh'
+
+    return 's' + 'uoh'
+}
+
+console.log(reverse('hous'));
+/*************** ****************/
+function reverse(string) {
+    if (string.length === 0) {
+        return '';
+    }
+
+    let sub_str = 'oh'
+
+    return 'u' + 'oh'
+}
+
+console.log(reverse('hou'));
+/*************** ****************/
+function reverse(string) {
+    if (string.length === 0) {
+        return '';
+    }
+
+    let sub_str = 'h'
+
+    return 'o' + 'h'
+}
+
+console.log(reverse('ho'));
+/*************** ****************/
+function reverse(string) {
+    if (string.length === 0) {
+        return '';
+    }
+
+    let sub_str = ""
+
+    return 'h' + ""
+}
+
+console.log(reverse('h'));
+/*************** ****************/
+function reverse(string) {
+    if (string.length === 0) {
+        return ''; // ""
+    }
+
+    let sub_str = reverse(string.slice(0, string.length - 1));
 
     return string[string.length - 1] + sub_str;
 }
 
-console.log(reverse_rec('lr'));
+console.log(reverse(''));
