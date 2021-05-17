06b592204ba7a3e2470d25c74731410cb5dd832f
Mylo James <mjames@appacademy.io>
2021-05-17 14:27:46 -0500

    reset for may

--- week3/d1-debugging/scratch.js
+++ week3/d1-debugging/scratch.js
@@ -1,8 +1,1 @@
-let firstName = 'Doctor';
-let lastName = 'Who';
-let opinion = 'worst';
 
-let fullName = `${firstName} ${lastName} is the ${opinion} show ever!!!!!!`;
-const appName = 'awesomeApp';
-
-console.log(fullName);
