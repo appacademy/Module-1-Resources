1f5119b42455eb0e96e952bee2945b926d5609eb
Mylo James <mjames@appacademy.io>
2021-05-18 09:44:21 -0500

    reset for may

--- week3/d1-debugging/scratch.js
+++ week3/d1-debugging/scratch.js
@@ -1,1 +1,6 @@
+let firstName = 'Doctor';
+let lastName = 'Who';
+// let sentence = 'Hello,' + " " + firstName + " " + lastName;
+let sentence = `Hello ${firstName} ${lastName}`;
 
+console.log(sentence);
