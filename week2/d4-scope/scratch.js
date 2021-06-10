db4e63b2f0eb5e30edb086c8e6198075ca20a9d9
Mylo James <mjames@appacademy.io>
2021-06-10 14:29:35 -0500

    formatting

--- week2/d4-scope/scratch.js
+++ week2/d4-scope/scratch.js
@@ -87,16 +87,23 @@ function hoistLetAndConst() {
 // console.log(reduceSum(arr));
 
 const roboBuilder = function (initString) {
-  console.log("Hey, I'm building a robot with the initSting of " + initString); // logged when roboBuilder is called
+  // logged when roboBuilder is called
+  console.log("Hey, I'm building a robot with the initSting of " + initString);
   return function innerRobo() {
-    console.log(initString + ", I'm a inner robot"); // logged when a 'version' of the inner function is invoked
+    // logged when a 'version' of the inner function is invoked
+    console.log(initString + ", I'm a inner robot");
   };
 };
 
-const robo1 = roboBuilder("Hello"); // This returns the innerFunction w/ initString = 'Hello';
-const robo2 = roboBuilder("Yoo"); // This returns the innerFunction w/ initString = 'Yoo';
+// Runs roboBuilder and returns the innerFunction w/ initString = 'Hello';
+const robo1 = roboBuilder("Hello");
+// Runs roboBuilder again and returns the innerFunction w/ initString = 'Yoo';
+const robo2 = roboBuilder("Yoo"); 
 
-console.log(robo1 === robo2); // They are NOT the same function, but two different 'versions' 
+// They are NOT the same function, but two different 'versions' 
+console.log(robo1 === robo2); 
 
-robo1(); // This invokes the first inner function we made on line 96
-robo2(); // This invokes the second inner function we made on line 97
+// This runs the first inner function we made on line 96
+robo1();
+// This runs the second inner function we made on line 97 
+robo2(); 
