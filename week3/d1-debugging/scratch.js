d5cc4082cfc6726f90c3e5ddb7f648d589b978fa
Mylo James <mjames@appacademy.io>
2021-07-13 08:15:59 -0500

    w3d1

---
+++ week3/d1-debugging/scratch.js
@@ -0,0 +1,6 @@
+let firstName = "Doctor";
+let lastName = "Who";
+// Hey, my name is Doctor Who, nice to meet you!
+let fullName = `Hey, my "name" is ${firstName.toUpperCase()} ${lastName}, it's nice to meet you!`;
+
+console.log(fullName);
