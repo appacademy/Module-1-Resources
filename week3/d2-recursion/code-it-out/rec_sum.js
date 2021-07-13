049449e88985dd0e3fecb9d3ba625a5a8e68bba6
Mylo James <mjames@appacademy.io>
2021-07-13 09:01:53 -0500

    fixed formatting

--- week3/d2-recursion/code-it-out/rec_sum.js
+++ week3/d2-recursion/code-it-out/rec_sum.js
@@ -3,7 +3,7 @@ Write a function called rec_sum that intakes a number, and adds up all
 numbers from 1 to the number and returns the number. Must be recursive.
 */
 function rec_sum(n) {
-
+  //our code here
 }
 
 console.log(rec_sum(3));
--- week3/d2-recursion/code-it-out/reverse_rec.js
+++ week3/d2-recursion/code-it-out/reverse_rec.js
@@ -2,6 +2,8 @@
 Write a function that intakes a string and returns the reverse of the string
 Must be recursive.
 */
-function reverse(string) {}
+function reverse(string) {
+  //our code here
+}
 
 console.log(reverse("lr")); // "rl"
