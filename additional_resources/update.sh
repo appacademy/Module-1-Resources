7692e014c2af439932911a54edc39423d6d07e89
Mylo James <mjames@appacademy.io>
2021-07-20 16:10:55 -0500

    fixed spacing issues

--- additional_resources/update.sh
+++ additional_resources/update.sh
@@ -2,38 +2,41 @@
 # Prompt user to insert inputs (one at a time)
 while true
 do
-    read -n 1 -p "What is the current WEEK?: " week
+    read -n 1 -p "What is the current WEEK?: `echo $'\n> '`" week
     if [ -z "$week" ]
     then 
-        echo 'Week cannot be blank please try again!'
+        echo "Week cannot be blank please try again! `echo $'\n '`"
         continue
     fi 
     if ! [[ "$week" =~ ^\s*[1-3]{1}\s*$ ]]
     then 
-        echo "Week must be number between 1 and 3"
+        echo
+        echo "Week must be number between 1 and 3 `echo $'\n '`"
         continue
     else 
         break
     fi
 done
-
+echo 
 while true
 do
-    read -p "What is the currnet DAY?: " day 
+    read -n 1 -p "What is the currnet DAY?: `echo $'\n> '`" day 
+
     if [ -z "$day" ]
     then 
-        echo 'Day cannot be blank please try again!' 
+        echo "Day cannot be blank please try again! `echo $'\n '`" 
         continue
     fi 
     if ! [[ "$day" =~ ^\s*[1-4]{1}\s*$ ]] 
     then
-        echo "Day must be a number between 1 and 4"
+        echo
+        echo "Day must be a number between 1 and 4 `echo $'\n '`"
         continue
     else
         break
     fi
 done
-
+echo
 cd ~/appacademy/Module-1-Resources &&
 git reset --hard &&
 git pull &&
