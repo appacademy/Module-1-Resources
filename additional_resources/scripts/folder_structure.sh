04437e85ff9e6b778383f6382991695e4176c4e2
mylo-james <zachary.henderson114@gmail.com>
2021-07-22 15:07:00 -0500

    remove /

--- additional_resources/scripts/folder_structure.sh
+++ additional_resources/scripts/folder_structure.sh
@@ -40,7 +40,7 @@ w3/d5/projects w3/d5/homework \
 
 if [ $SHELL = '/bin/bash' ]; then
     if [ -e $HOME/.bash_profile ]; then
-        BASHRC_IN_BASH_PROFILE=$(cat $HOME/.bash_profile | grep -c '\$HOME/.bashrc')
+        BASHRC_IN_BASH_PROFILE=$(cat $HOME/.bash_profile | grep -c '$HOME/.bashrc')
         if [ -e $HOME/.bashrc ] && [ $BASHRC_IN_BASH_PROFILE != 1 ]; then
             ALIAS_IN_START=$(cat $HOME/.bashrc | grep -c 'https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh')
             if [ $ALIAS_IN_START != 1 ]; then
@@ -55,7 +55,7 @@ if [ $SHELL = '/bin/bash' ]; then
         fi
         exit 0
     elif [ -e $HOME/.profile ]; then
-        BASHRC_IN_PROFILE=$(cat $HOME/.profile | grep -c '\$HOME/.bashrc')
+        BASHRC_IN_PROFILE=$(cat $HOME/.profile | grep -c '$HOME/.bashrc')
         if [ -e $HOME/.bashrc ] && [ $BASHRC_IN_PROFILE != 1 ]; then
             ALIAS_IN_START=$(cat $HOME/.bashrc | grep -c 'https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh')
             if [ $ALIAS_IN_START != 1 ]; then
