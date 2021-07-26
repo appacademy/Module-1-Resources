3dbcd4dfbddcbd71e21992d0ef710086e01b02ed
Mylo James <mjames@appacademy.io>
2021-07-26 17:16:02 -0500

    july day one

---
+++ w1/d1/scratch.js
@@ -0,0 +1,104 @@
+/* 
+Active Learning: Learning by DOING
+Passive Learning: Learning by reading/watching...
+Spaced Repetition: Learning over time (not cramming)
+*/
+
+// name is equal to the user's name...
+/* 
+this is 
+a 
+muli-line
+comment
+*/
+let name = "Mylo";
+
+// justin
+
+//A bunch of code.....
+
+// console.log(name);
+
+// console.log(1 + 2); //3
+// console.log(-1 / 2); // -.5
+// console.log(1.1 * 2); /// 2.2
+
+// // Order of operations - PE(MMD)(AS)
+
+// console.log((1 + 3) * 4) //16
+
+// // Modulo - return the remainder / left over
+
+// console.log(15 % 2); // 1
+// console.log(7 % 4); // 3
+// console.log(4 % 7); // 4
+
+// console.log(16 % 2);
+
+// console.log(2 ** 4); // exponent
+
+// console.log(2 % 2 / 2 * 2)
+//Booleans
+
+// console.log(!true);
+
+// console.log(true && true); // true
+// console.log(false && true); // false
+// console.log(true && false); // false
+// console.log(false && false); // false
+
+// console.log(false || false); //false
+// console.log(true || false); // true
+// console.log(false || true); // true
+// console.log(true || true); // true
+
+//DeMorgan's law
+// !(A && B) => !A || !B
+
+//Variables
+
+// let animal; //Variable Declaration
+// animal = 'elephant'; // Variable Assignment/reassignment
+
+// let game = 'pokemon'; // Variable Initialzation
+// let kitchen = 'plates';
+// // <= name of the box | => what to put in the box
+// console.log(animal);
+// console.log(game);
+
+// < > <= >= == ===
+
+// console.log(1 < 2); //true
+// console.log(2 > 1); //true
+// console.log(2 >= 2); //true
+// console.log(2 <= 2); //true
+
+// console.log('a' < 'b'); //true
+// console.log('app' < 'apple'); //true
+// console.log('a' < 'Z'); //false (turn in to lowercase)
+
+// let a = 2;
+// let b = 3;
+
+// console.log(a > b); // 2 > 3
+
+// = assignment (RED FLAG HERE)
+// == loose equality
+// === deep equality
+
+// console.log(2 == '2');
+// console.log(2 === '2');
+// console.log(2 === Number('2')); //change a string to a number
+// console.log(2 != '2'); //false
+// console.log(2 !== '2'); //true
+
+//is something even...
+
+let num = 4; //Varibale init...
+
+/* 
+if num is even, then I want to
+print true
+otherwise I want to print false
+*/
+console.log(num % 2 === 0);
