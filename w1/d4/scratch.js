9e952d89eec8c6863b0a37766a45cc92e4318189
Mylo James <mjames@appacademy.io>
2021-07-29 11:23:15 -0500

    day 4 done

---
+++ w1/d4/scratch.js
@@ -0,0 +1,47 @@
+// //Helper Function
+
+// /*
+// "Hey, there's going to be a user input and we need to makes sure
+// that what the user gives us is a number between 1 - 60."
+
+// "Hey, there's going to be ANOTHER user inpuut and this one we need to
+// make sure the input is a number between 5 - 12"
+
+// Helper function
+//   intakes two numbers and an input and sees if the numbers are between the inputs
+//   if so, return true else return false.
+// */
+
+// let userInput = "13";
+
+// let betweenTwoNums = function(min, max, input) {
+//   let numInput = Number(input);
+//   if (numInput >= min && numInput <= max) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// let betweenOneAndSixty = function(input){
+//   if (betweenTwoNums(1, 60, input)) {
+//   	return input;
+//   } else {
+//     return "Not a number between 1 and 60"
+//   }
+// }
+
+// let betweenFiveAndTwelve = function (input) {
+//   if (betweenTwoNums(5, 12, input)) {
+//     return input
+//   } else {
+//     return "NAAAH";
+//   }
+// }
+
+// console.log(betweenOneAndSixty(userInput));
+// console.log(betweenFiveAndTwelve(userInput));
+
+// let userInput = '12';
+
+// console.log(typeof Number(userInput))
