0c3708a9f0fd286509f78fe63e7cc15ab39afb96
mylo-james <zachary.henderson114@gmail.com>
2021-07-30 22:35:22 -0500

    refactored logic, added Peter and Senyo

--- additional_resources/scripts/folder_structure.sh
+++ additional_resources/scripts/folder_structure.sh
@@ -1,77 +1,40 @@
 #!/bin/bash
-cd $HOME &&
-mkdir appacademy && 
-cd appacademy && 
-git clone -q https://github.com/appacademy/Module-1-Resources.git && 
-mkdir \
-w1 \
-w1/d1 \
-w1/d1/projects w1/d1/homework \
-w1/d2 \
-w1/d2/projects w1/d2/homework \
-w1/d3 \
-w1/d3/projects w1/d3/homework \
-w1/d4 \
-w1/d4/projects w1/d4/homework \
-w1/d5 \
-w1/d5/projects w1/d5/homework \
-w2 \
-w2/d1 \
-w2/d1/projects w2/d1/homework \
-w2/d2 \
-w2/d2/projects w2/d2/homework \
-w2/d3 \
-w2/d3/projects w2/d3/homework \
-w2/d4 \
-w2/d4/projects w2/d4/homework \
-w2/d5 \
-w2/d5/projects w2/d5/homework \
-w3 \
-w3/d1 \
-w3/d1/projects w3/d1/homework \
-w3/d2 \
-w3/d2/projects w3/d2/homework \
-w3/d3 \
-w3/d3/projects w3/d3/homework \
-w3/d4 \
-w3/d4/projects w3/d4/homework \
-w3/d5 \
-w3/d5/projects w3/d5/homework \
-
+cd $HOME
+# Creates an appacademy folder if there isn't one already
+mkdir -p appacademy
+cd appacademy
+# Create a Mod1 Resource if it doesn't exist or pull it
+if [ ! -d Module-1-Resources ]; then
+  git clone -q https://github.com/appacademy/Module-1-Resources.git
+else
+  cd Module-1-Resources
+  git pull -q
+  cd ../
+fi
+# Make the Mod1 Folder Structure
+mkdir -p {w1,w2,w3}/{d1,d2,d3,d4,d5}/{projects,homework}
+# Find the correct startup file
 if [ $SHELL = '/bin/bash' ]; then
     if [ -e $HOME/.bash_profile ]; then
-        BASHRC_IN_BASH_PROFILE=$(cat $HOME/.bash_profile | grep -c 'source $HOME/.bashrc')
-        if [ -e $HOME/.bashrc ] && [ $BASHRC_IN_BASH_PROFILE > 0 ]; then
-            ALIAS_IN_START=$(cat $HOME/.bash_profile | grep -c 'https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh')
-            if [ $ALIAS_IN_START != 1 ]; then
-                echo -e "\nalias aa_update='curl -s https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh | bash'" >> $HOME/.bash_profile
-            fi
-        else
-            ALIAS_IN_START=$(cat $HOME/.bashrc | grep -c 'https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh')
-            if [ $ALIAS_IN_START != 1 ]; then
-                echo -e "\nalias aa_update='curl -s https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh | bash'" >> $HOME/.bashrc
-            fi
-            
-        fi
-        exit 0
+        PROFILE_FILE='.bash_profile'
     elif [ -e $HOME/.profile ]; then
-        BASHRC_IN_PROFILE=$(cat $HOME/.profile | grep -c 'source $HOME/.bashrc')
+        PROFILE_FILE='.profile'
+    else
+        PROFILE_FILE='.bashrc'
+    fi
+    # Check if the .bashrc is loaded in the startup file, if so we'll use the .bashrc
+    if [ $PROFILE_FILE != '.bashrc' ]; then
+        BASHRC_IN_BASH_PROFILE=$(cat $HOME/$PROFILE_FILE | grep -c 'source $HOME/.bashrc')
         if [ -e $HOME/.bashrc ] && [ $BASHRC_IN_PROFILE > 0 ]; then
-            ALIAS_IN_START=$(cat $HOME/.profile | grep -c 'https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh')
-            if [ $ALIAS_IN_START != 1 ]; then
-                echo -e "\nalias aa_update='curl -s https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh | bash'" >> $HOME/.profile
-            fi
-        else
-            ALIAS_IN_START=$(cat $HOME/.bashrc | grep -c 'https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh')
-            if [ $ALIAS_IN_START != 1 ]; then
-                echo -e "\nalias aa_update='curl -s https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh | bash'" >> $HOME/.bashrc
-            fi
+            PROFILE_FILE='.bashrc'
         fi
-        exit 0;
     fi
 elif [ $SHELL = '/bin/zsh' ]; then
-    ALIAS_IN_START=$(cat $HOME/.zshrc | grep -c 'https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh')
-    if [ $ALIAS_IN_START != 1 ]; then
-        echo -e "\nalias aa_update='curl -s https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh | bash'" >> $HOME/.zshrc
-    fi
+    PROFILE_FILE='.zshrc'
+fi
+# Check if the alias already exists
+ALIAS_IN_START=$(cat $HOME/$PROFILE_FILE| grep -c 'https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh')
+# If not append it to the file
+if [ $ALIAS_IN_START != 1 ]; then
+    echo -e "\nalias aa_update='curl -s https://raw.githubusercontent.com/appacademy/Module-1-Resources/main/additional_resources/scripts/update.sh | bash'" >> $HOME/$PROFILE_FILE
 fi
