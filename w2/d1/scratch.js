3409a90a0a3669e8c9e36153d2ff5cc9c1f48c06
jeff <jlgranof@icloud.com>
2021-11-01 15:31:51 -0500

    lecture

--- w2/d1/scratch.js
+++ w2/d1/scratch.js
@@ -1,9 +1,9 @@
-// let arr = [1,2,3];
+let arr = [1,2,3];
 
-// for (let i = 0; i < arr.length; i++) {
-//   let ele = arr[i];
-//   console.log(ele);
-// }
+for (let i = 0; i < arr.length; i++) {
+  let ele = arr[i];
+  console.log(ele);
+}
 
 // Array.forEach()
 
