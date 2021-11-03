8173297aa1ff8ae9456186b679ba6091f4bcb6e5
jeff <jlgranof@icloud.com>
2021-11-03 10:21:11 -0500

    lecture

--- w3/d3/scratch.js
+++ w3/d3/scratch.js
@@ -3,16 +3,16 @@
 // Keep the global namespace clean
 
 /*
-1. Declare a function
-2. Use immeidately after
-3. Never going to use the function again
+    1. Declare a function
+    2. Use immediately after
+    3. Never going to use the function again
 */
 
 // let run = (name) => {
 //     console.log(`start the program, ${name}`);
 // }
 
-// run("Bill")
+// run("Bill");
 
 ((name) => {
     console.log(`start the program, ${name}`);
