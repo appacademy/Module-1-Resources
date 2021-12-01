167f9e8c16e5a358dc4a411108e518436861f002
jeff <jlgranof@icloud.com>
2021-12-01 15:32:39 -0600

    lecture

---
+++ w3/d1/scratch.js
@@ -0,0 +1,49 @@
+// let name = "Nick";
+// let isCool = false;
+
+// console.log(name + " is cool: " + isCool);
+
+// console.log(`${name} is cool: ${isCool}`);
+
+// let arr = [
+//   { name: "Jeff" },
+//   { name: "Caleb" },
+//   { name: "Drew" }
+// ];
+
+// arr.forEach(obj => console.log(`This persons name is ${obj.name}`));
+
+// debugger;
+// console.log('hi');
+
+// let arr = [1,2,3,4,5]
+// let newArr = [];
+
+// debugger;
+// for (let i = 0; i < arr.length; i++) {
+//   debugger;
+//   let el = arr[i];
+//   let newEl = el * 2;
+//   newArr.push(newEl);
+// }
+
+// console.log(newArr);
+
+function nick() {
+  debugger;
+  console.log("nick");
+  matt();
+}
+
+function matt() {
+  debugger;
+  console.log("matt");
+  noel();
+}
+
+function noel() {
+  debugger;
+  console.log("noel")
+}
+
+nick();
