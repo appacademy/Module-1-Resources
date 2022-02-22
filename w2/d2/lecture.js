ee6a0a7931b7f520ec693673753da4e6c9166e6b
jeff <jlgranof@icloud.com>
2022-02-22 14:52:55 -0600

    tuesday lecture

--- w2/d2/lecture.js
+++ w2/d2/lecture.js
@@ -9,116 +9,103 @@
 //   },
 //   numberOfRogues: 5,
 //   goodTrilogy: false,
-//   qualityOrder: [2, 1, 3],
-// };
+//   qualityOrder: [2, 1, 3], 
+// }
 
-// access the name
+// access name
 // console.log(series["name"]);
 // console.log(series.name);
 
-// access movies
+// access the movies
 // console.log(series["movies"]["first"]);
 // console.log(series["movies"].second);
 // console.log(series.movies.third);
-
 
 // change goodTrilogy to true
 // series.goodTrilogy = true;
 // console.log(series);
 
-
-// increment the number of rogues since we forgot Talia
+// incremement numberOfRogues to include Talia
 // series.numberOfRogues++;
 // console.log(series);
 
 
 // key into the array inside of the object
-// console.log(series.qualityOrder[0]);
+// console.log(series.qualityOrder[2]);
 
 
-// add if favorite line key/value pair
-// series.favoriteQuote = "Swear to me.";
-// console.log(series);
+// add in Jeffs favorite line as a new key/value pair
+// series.favortieQuote = "Swear to me.";
+// console.log(series); 
 
 
-
-// // using variables
+// using variables
 // let obj = {
-//   school: 'Wisconsin',
-//   basketballTeam: "Decent",
+//   school: "Wisconsin",
+//   basketballTeam: "Pretty Decent",
 //   location: "Madison",
 // }
 
 // let someVariable = "school";
 // let some = "basketball";
 // let variable = "Team";
-// let place = "LocAtiON";
+// let place = "LoCaTiOn";
 
-// // console.log(obj[someVariable]);
-// // console.log(obj[some + variable]);
-// // console.log(obj[place.toLowerCase()]);
+// console.log(obj[someVariable]);
+// console.log(obj[some + variable]);
+// console.log(obj[place.toLowerCase()]);
 
-
-// // Check if a key is in an object
-// console.log(obj.place)
-
-// // if (obj.school !== undefined) console.log("This is in the object")
-
-// if ("location" in obj) console.log("This is also in the object")
+// if (obj.school !== undefined) console.log("This is in the object");
+// if (someVariable in obj) console.log("This is in the object");
 
 
-
-// let series = {
+// let show = {
 //   name: "Dragonball",
-//   shows: ["OG", "Z", "GT", "Super"],
+//   series: ["OG", "Z", "GT", "Super"],
 //   characters: {
 //     main: "Goku",
 //     son: "Gohan",
 //   },
-// };
-
+// }
 
 // // for in loop
-// // for (let key in series) {
+// // for (let key in show) {
 // //   console.log(key);
-// //   console.log(series[key])
+// //   console.log(show[key]);
 // // }
 
+// // Getting All of the Keys
+// let keyArray = Object.keys(show);
+// // console.log(keyArray);
 
-// // Get all the keys
-// let keyArray = Object.keys(series);
-// console.log(keyArray);
+// // Getting All of the Values
+// let valueArray = Object.values(show);
+// // console.log(valueArray);
 
-// // Get all the values
-// let valueArray = Object.values(series);
-// console.log(valueArray);
-
-// // Get all the entries
-// let entryArray = Object.entries(series);
+// // Getting all the entries
+// let entryArray = Object.entries(show);
 // console.log(entryArray);
+// // console.log(show)
 
 
-
-// // Primitives
+// Primitives
 // let age = 56;
 // let newAge = age;
-// age++;
+// age = newAge + 1;
 
 // console.log(age, newAge);
 
 
-// // Reference 
+// // Reference
 // let biff = {
-//   type: "cat",
+//   type: 'cat',
 //   age: 16,
-// }
+// };
 
 // let buster = biff;
+// biff.age = 3;
 
-// buster.age = 3;
 // console.log(biff, buster);
-
-
 
 
 // Destructuring
@@ -127,6 +114,7 @@
 //   name: "Ani",
 //   homeWorld: "Tatooine",
 //   species: "Human",
+//   lightsaberColor: "blue",
 //   isMaster: false,
 //   favoriteThings: {
 //     person: "Padme",
@@ -134,49 +122,41 @@
 //   },
 // }
 
-// // let { name, homeWorld, species, isMaster, favoriteThings } = character;
-// // console.log(name, homeWorld, species, isMaster, favoriteThings);
+// // let { name, homeWorld, species, lightsaberColor, isMaster, favoriteThings } = character;
+// // console.log(name, homeWorld, species);
 
-// // let { name: aniName, homeWorld: world, species } = character;
-// // console.log(aniName, world, species);
-// // console.log(name)
+// // let { name: aniName, homeWorld: world } = character;
+// // console.log(aniName, world);
 
 // let { favoriteThings: { person, activity } } = character;
-// // console.log(favoriteThings) // cannot access
 // console.log(person, activity);
-
-// // what line 144 is doing
-// let { favoriteThings: things } = character;
-// let { person, activity } = things;
-
 
 
 // let arr = ["Kristen", "Jeff", "Javier"];
 // // let [first, second, third] = arr;
+
 // // console.log(first, second, third);
 
 // console.log(arr);
 // [arr[0], arr[1]] = [arr[1], arr[0]];
 // console.log(arr);
 
-
-// // Switch some variables
+// Swap some variables
 // let lastName = "Jeff";
-// let firstName = "Granof"; 
+// let firstName = "Granof";
 
 // [lastName, firstName] = [firstName, lastName];
 // console.log(firstName, lastName);
 
 
-
 // Rest operator
 
-// let yeller = function (...strings) {
-//   let result = strings.map(function (string) {
-//     return string.toUpperCase();
+// let yeller = function (...words) {
+//   let result = words.map(function (word) {
+//     return word.toUpperCase();
 //   });
 //   return result.join(" ") + "!";
-// }
+// };
 
 // console.log(yeller("i", "am", "the", "senate"));
 
@@ -184,17 +164,17 @@
 // Spread Operator
 // let arr1 = [1, 2, 3];
 // let arr2 = [4, 5, 6];
-// let arr3 = [0, arr1, arr2]
-// console.log(arr3);
+// let arr3 = [0, arr1, arr2];
+// console.log(arr3)
 
 // let arr4 = [0, ...arr1, ...arr2];
 // console.log(arr4);
 
 
 let obj1 = { name: "Jeff" };
-let obj2 = { animal: 'dog', game: 'Brilliant Diamond' };
-let obj3 = { obj1, obj2, movie: 'Scream' };
+let obj2 = { animal: "dog", game: "Arceus" };
+let obj3 = { obj1, obj2, movie: "No Way Home" };
 console.log(obj3);
 
-let obj4 = { ...obj1, ...obj2, movie: 'Scream' };
+let obj4 = { ...obj1, ...obj2, movie: "No Way Home" };
 console.log(obj4);
