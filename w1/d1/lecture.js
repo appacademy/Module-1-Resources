49c4f8146b211873dcb77aaac7cd9f63ba723a7a
jeff <jlgranof@icloud.com>
2022-03-14 17:17:08 -0500

    first day lecture

---
+++ w1/d1/lecture.js
@@ -0,0 +1,123 @@
+// How to Learn
+
+// Active Learning: doing an exercise, taking a test, building a project, debugging
+
+// Passive Learning: watching a video, reading an article, taking notes while Jeff lectures
+
+
+// Intro to Debugging
+
+// This is a single line comment, using two slashes
+// let name = "Jeff"; // I created a variable with "Jeff" as the value
+
+/* 
+this
+is
+a
+multi line comment
+*/
+
+// console.log("Hello, World!");
+
+// let name = "Jeff";
+// console.log(name);
+
+
+
+// Number Type
+// console.log(12 + 2); // 14
+// console.log(7 - 9); // -2
+// console.log(2.02 * 3); // 6
+// console.log(8 / 2); // 4
+
+// Order of Operations
+// PE => MMD => AS
+
+// console.log(1 + 3 * 4); // 13
+// console.log((1 + 3) * 4); // 16
+
+// console.log(5 ** 2); // 25
+// console.log(2 ** 5); // 32
+
+
+// Modulo Operator
+// console.log(15 % 5); // 3r0 => 0
+// console.log(16 % 5); // 3r1 => 1
+// console.log(17 % 5); // 3r2 => 2
+// console.log(18 % 5); // 3r3 => 3
+// console.log(19 % 5); // 3r4 => 4
+// console.log(20 % 5); // 4r0 => 0
+
+
+// console.log(5 % 15); // 0r5 => 5
+
+
+// Booleans
+// console.log(true);
+// console.log(false);
+
+// Not Operator (!) - bang
+// console.log(!true);
+// console.log(!false);
+
+
+// And Operator (&&)
+// console.log(true && true);
+// console.log(false && true);
+// console.log(false && false);
+
+
+// Or operator (||) 
+// console.log(true || true);
+// console.log(false || true);
+// console.log(false || false);
+
+
+// DeMorgan's Law
+// !(A && B) => !A || !B
+// !(A || B) => !A && !B
+
+
+// Variables
+// let animal; // Variable Declaration
+// console.log(animal);
+// animal = "cat"; // Variable Assignment
+// console.log(animal);
+// animal = "chicken"; // Variable Reassignment
+// console.log(animal);
+
+// let animal2 = "dog"; // Variable Initialization
+// console.log(animal2);
+
+// let animal2 = "pig"; // CANNOT REDECLARE THIS VARIABLE
+
+
+// Comparison Operators
+// < > <= >= == ===
+
+// console.log(2 <= 2);
+// console.log(2 < 1);
+
+// console.log('a' < 'b');
+// console.log('app' < 'apple');
+// console.log('a' < 'Z');
+
+// 'A' < 'Z' < 'a' < 'z'
+
+
+// let a = 4;
+// let b = 3;
+
+// console.log(a > b); // 4 > 3 => true
+
+
+// Equality
+// = assignment operator 
+// == loose equality
+// === deep equality
+// != not loose equality
+// !== not deep equality
+
+// console.log(2 == '2');
+// // console.log(2 === '2');
+// console.log(2 === Number('2'));
