432e5415838a37dbd4b0980e6f1f76f42d4b5cd0
Bill Adams <vikingbill@gmail.com>
2022-04-21 10:30:29 -0400

    add loop suffix

--- w2/d3/eod.js
+++ w2/d3/eod.js
@@ -5,19 +5,43 @@ let myEvery = function(arr, cb) {
     return true
 };
 
-let suffixCipher = function (sentence, cipher) {
-    let res = []
-    let words = sentence.split(" ")
-    words.forEach(function (word) {
+// let suffixCipher = function (sentence, cipher) {
+//     let res = []
+//     let words = sentence.split(" ")
+//     words.forEach(function (word) {
+//         for (let suffix in cipher) {
+//             if (word.endsWith(suffix)) {
+//                 res.push(cipher[suffix](word))
+//                 return
+//             }
+//         }
+//         res.push(word)
+//     })
+//     return res.join(" ")
+// };
+
+let suffixCipher = function(sentence, cipher) {
+    let res = [];
+
+    let words = sentence.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        let isSuffix = false;
         for (let suffix in cipher) {
             if (word.endsWith(suffix)) {
                 res.push(cipher[suffix](word))
-                return
+                isSuffix = true;
+                console.log(res);
+                break;
             }
         }
-        res.push(word)
-    })
-    return res.join(" ")
+        if (!isSuffix) {
+            res.push(word)
+        }
+    }
+
+    return res.join(' ')
 };
 
 let minValueCallback = function (nums, callback) {
