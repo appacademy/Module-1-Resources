82e1ab1af6093cdd14caf55286b45fa5d92c09a9
Bill Adams <vikingbill@gmail.com>
2022-04-20 20:08:25 -0400

    add eod file, lecture notes

---
+++ w2/d3/eod.js
@@ -0,0 +1,33 @@
+let myEvery = function(arr, cb) {
+    for (let element of arr){
+        if (!cb(element)) return false
+    }
+    return true
+};
+
+let suffixCipher = function (sentence, cipher) {
+    let res = []
+    let words = sentence.split(" ")
+    words.forEach(function (word) {
+        for (let suffix in cipher) {
+            if (word.endsWith(suffix)) {
+                res.push(cipher[suffix](word))
+                return
+            }
+        }
+        res.push(word)
+    })
+    return res.join(" ")
+};
+
+let minValueCallback = function (nums, callback) {
+    return nums.reduce(function (lowest, current) {
+        if (callback) {
+            if (lowest > callback(current)) return callback(current)
+            return lowest
+        } else {
+            if (lowest > current) return current
+            return lowest
+        }
+    }, Infinity)
+};
