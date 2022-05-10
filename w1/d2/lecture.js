9dd80788ec684dd94dce746bf56ac9f051da5325
Bill Adams <vikingbill@gmail.com>
2022-05-10 12:28:43 -0400

    after strings lecture

---
+++ w1/d2/lecture.js
@@ -0,0 +1,27 @@
+// your code here
+// console.log('"Join a bootcamp," they said. "It will be fun," they said.')
+// console.log('\W\hat\'s up?\\');
+
+// let greeting = "hello";
+
+// console.log(greeting.length);
+// console.log("hell\\"o".length);")
+// console.log("".length);
+
+
+// let animal = "CATS"
+
+// console.log(animal[0] + animal[1] + animal[2])
+// console.log(animal[0].concat(animal[1], animal[2])+animal[3])
+// console.log(animal[1])
+// console.log(animal[2])
+// console.log(animal[3])
+01234567
+let playoffs = "playoffs play"
+
+// console.log(playoffs.indexOf("a"))
+// console.log(playoffs.indexOf("f"))
+// console.log(playoffs.indexOf("f",6))
+let firstIndex = playoffs.indexOf("play")
+console.log(playoffs.indexOf("play", firstIndex + 1))
+console.log(playoffs.indexOf("Sabres"))
