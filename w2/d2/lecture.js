4205ef8068450f693635b5569a16dc998f2080ed
Bill Adams <vikingbill@gmail.com>
2022-05-17 12:51:12 -0400

    end pojo lecture

--- w2/d2/lecture.js
+++ w2/d2/lecture.js
@@ -19,3 +19,91 @@ Play around with objects for a moment and start to get very comfortable with how
 we can use either dot or bracket notation. Objects are going to be one of (if 
 not the) most important things you'll learn in this bootcamp. 
 */
+
+// let myObj = {}
+
+// console.log(myObj)
+
+// myObj.favColor = "green"
+
+// console.log(myObj)
+
+// myObj.favFood = "pizza"
+
+// console.log(myObj)
+
+// myObj["favInstrument"] = "guitar"
+
+// console.log(myObj)
+
+
+// let books = {
+//    first: "Way of Kings",
+//    second: "Words of Radiance",
+//    third: "Oathbringer",
+//    fourth: "Rhythm of War"
+// }
+
+// const series = {
+//    name: "Stormlight Archives",
+//    author: "Brandon Sanderson",
+//    numberOfNovellas: 1,
+//    worthReading: false,
+//    banana: books,
+// }
+
+// let color = "blue"
+
+// // series.banana.first = "This is a test"
+// series[color] = "Blue"
+
+// // console["log"](books)
+// console.log(series)
+// console.log(books)
+// console.log(books.first["length"])
+// console.log(series)
+// let myArr = [1,2,3]
+// let banana = "log"
+// let papaya = "map"
+// // console[banana](myArr[papaya](el => el * 2))
+// console.log(myArr.map(el => el * 2))
+
+let show = {
+   name: "Star Trek",
+   series: ["TOS", "TNG", "DS9", "VOY"],
+   captains: {
+      TOS: "Kirk",
+      TNG: "Picard",
+      DS9: "Sisko",
+      VOY: "Janeway"
+   },
+   myFunc: () => console.log("Hi!")
+}
+
+const myArr = [() => console.log("Hi!"), 2, 3, 4]
+// myArr[0]()
+show.myFunc()
+//  show.doctors = show.captains
+//  console.log(show)
+//  delete show.captains
+//  console.log(show)
+// let keys = Object.keys(show)
+// console.log(keys)
+// keys.forEach(key => console.log(show[key]))
+
+// let values = Object.values(show)
+// console.log(values)
+
+// for (let key in show){
+//    console.log(key)
+//    let value = show[key]
+//    console.log("     ",value)
+//    // if (Array.isArray(value)){
+//    //    value.forEach(el => console.log(el))
+//    // }
+//    // if (typeof value === "object" && !Array.isArray(value)){
+//    //    for (let show in value){
+//    //       console.log(value[show])
+//    //    }
+//    // }
+// }
