9adede4d5b6c738237a62a2835959c9ca877a4e9
Bill Adams <vikingbill@gmail.com>
2022-05-17 11:22:19 -0400

    add object practice

---
+++ w2/d2/lecture.js
@@ -0,0 +1,21 @@
+/*
+Create an object and store it in a variable (perhaps myObj). Once you have your
+object created, add some keys using dot notation:
+ -- favFood = your favorite food
+ -- favNumbr = your favorite number(or age)
+ -- birthDay = your birthday
+
+Check to make sure things are working like you expect with a console.log
+statement. After you're feeling pretty good about dot notation, let's try it
+with brackets.
+
+-- change all of your above statements to instead use bracket notation
+-- next, define a variable and give it some value, then use that variable to 
+   create a new key value pair.
+-- change the value stored in your variable, see how it alters what key/value is
+   added to your object.
+
+Play around with objects for a moment and start to get very comfortable with how
+we can use either dot or bracket notation. Objects are going to be one of (if 
+not the) most important things you'll learn in this bootcamp. 
+*/
