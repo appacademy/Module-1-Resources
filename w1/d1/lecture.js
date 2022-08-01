27cbfc2ec8eaf4e52d81ea25933090ae90a95c59
Bill Adams <vikingbill@gmail.com>
2022-08-01 18:23:26 -0400

    end of lecture, w1d1

---
+++ w1/d1/lecture.js
@@ -0,0 +1,141 @@
+// // // What is learning?
+// // // Process of acquiring skills and knowledge to be used at a later time
+// // // Being an active learner, being engaged in  material and getting feedback
+
+// // // The acquisition of skills through experience or study and the ability to 
+// // // recall and apply them in the future
+
+// // // Active and Passive learning: Which is better?
+// // // Active learning -- apply the skills that we are learning helps us engage with
+// // // material and lets us get feedback on our progress
+
+// // // Desireable difficulty?
+// // // Want to have something that is challenging enough that its engaging and 
+// // // pushing but not overly so that it is discouraging
+
+// // // Please ask questions!!
+// // // If you've spent more than 15 minutes of effort on a single problem, raise your
+// // // hand in PT
+
+// // // Spaced Repitition
+// // // 
+
+// // // What is debugging?
+// // // The process of identifying an error in our coude and going about solving the
+// // // error. 
+
+// // // Should we be afraid to make mistakes?
+// // // 
+
+// // // If we make a mistake, how can we debug?
+
+// // // Error messages and console.log statements
+
+// // // let message = "Hello!"
+// // // console.log(message)
+// // // //
+
+// // // Data Types:
+
+// // // Number Type:
+
+// // // PEMMDAS
+
+// // // PE => MMD => AS
+
+// // // console.log("+++++++++++", 12 + 2)
+// // // console.log((1 + 3) * 4)
+// // // console.log(5 ** 2)
+// // // console.log(2.02 * 3)
+
+// // // console.log(5 % 12) // what should we get?
+
+// // // Boolean type
+// // // true
+// // // false
+
+// // // console.log(!true)
+// // // ! operator
+
+// // // console.log(true && !false)
+// // // console.log(!true || false)
+// // // && - multiplication
+// // // || - addition
+
+// // // DeMorgan's Law
+// // // !(A && B) => !A || !B
+// // let a = false
+// // let b = true
+// // console.log("Logical and statement:", !(a && b))
+// // console.log("Logical or statement: ",!a || !b)
+
+// // Variables?
+// // Variable is a value that is stored in some named reference for use later
+// // !equals
+
+// // = is assignment operator
+// // == or === 
+// // let pet;
+
+// // console.log(pet)
+
+// // pet = "cat";
+
+// // console.log(pet)
+
+
+
+// // {
+// //     let pet2 = "dog"
+// // }
+// // console.log(pet2)
+
+
+
+// let myPet = "cat";
+
+// console.log(myPet)
+
+// myPet = "dog";
+
+// console.log(myPet)
+
+// let age = 20;
+// console.log(age)
+// // console.log(age + 5)
+// // age = age + 5
+// age += 5
+// console.log(age)
+// age /= 5 
+// console.log(age)
+// age *= 7 
+// console.log(age)
+
+let age = 20
+// age++
+// // age += 1
+// // age = age + 1
+// console.log(age)
+// age--
+// console.log(age)
+// let animal = "cat"
+// let animal2 = "dog"
+// console.log(age * animal)
+// // 2 vs "2"
+
+// comparison operators
+// console.log(2 == 2.0)
+// comparing strings!
+// A < Z < a < z
+// console.log('app' < 'Zebra')
+// let a = 3
+// let b = 6
+// console.log(a > b)
+
+// Equality
+// === vs ==
+// console.log(typeof 2)
+// console.log(typeof '2')
+// // console.log(2 === '2')
+// console.log(2 === Number('2'))
+console.log(-1 < 1)
