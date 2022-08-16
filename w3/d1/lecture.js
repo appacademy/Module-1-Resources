2fa7fd730afc10a438f31fd8377e8c7c010e3900
Bill Adams <vikingbill@gmail.com>
2022-08-16 16:36:01 -0400

    end day 2

---
+++ w3/d1/lecture.js
@@ -0,0 +1,56 @@
+// // string interpolation!
+
+// let firstName = "Bill"
+// let lastName = "Adams"
+
+// // console.log("My first name is " + firstName + " and my last name is " + lastName)
+
+// // ` ` 
+// // console.log(`My first name is ${firstName} and my last name is ${lastName}`)
+
+// const firstArr = ["Jojo", `Jesse`, 'Braxton', "Charlie", ]
+// const lastArr = [`Yih`, "Christensen", 'Kappes', `Sheppard`]
+
+// // firstArr.forEach((firstName, i) => console.log(`${firstName} ${lastArr[i]} is on the mod 1 team!`))
+
+// // console.log(`${firstArr.reduce((allNames, currentName, index) => {
+// //     allNames.push(`${currentName} ${lastArr[index]}`)
+// //     return allNames
+// // }, []).join(" and ")} are the Mod 1 Instructional Assistants!`)
+
+// let firstAndLastNames = firstArr.map((currentFirstName, index) => 
+// `${currentFirstName} ${lastArr[index]}`)
+
+// console.log(`${firstAndLastNames.join(" and ")} are the Mod 1 Instructional Assistants!`)
+
+// // for (let i = 0; i < firstArr.length; i++){
+// //     console.log(`${firstArr[i]} ${lastArr[i]} is on the Mod 1 Team!`)
+// // }
+
+// function foo(word){
+//     debugger
+//     console.log(word)
+//     bar("barber")
+//     debugger
+// }
+
+// function bar(word){
+//     debugger
+//     console.log(word)
+//     baz("bazaar")
+//     debugger
+// }
+
+// function baz(word){
+//     debugger
+//     console.log(word)
+//     debugger
+// }
+
+// foo("food")
+const test = []
+for (let i = 0; i<10; i++){
+    debugger
+    test.push(i)
+    debugger
+}
--- w3/d2/lecture.js
+++ w3/d2/lecture.js
@@ -1,1 +1,36 @@
+//recursion!
+// Write a recursive function reverse(string) that takes in a string and returns
+// that string reversed
 
+// given some string, write a recursive function that reverses and returns that string
+
+
+function reverse(string){ // define function
+    if (!string) return string // define base case
+    return `${reverse(string.slice(1))}${string[0]}` //define recursive step
+}
+
+function betterReverse(string){
+    debugger
+    if (string.length === 0) return ""
+    // console.log(string)
+    debugger
+    const first = string[0]
+    // console.log(first)
+    debugger
+    let recurse = betterReverse(string.slice(1))
+    // console.log(result)
+    debugger
+    let result = recurse + first
+    // console.log(result)
+    debugger
+    return result
+}
+
+
+
+// console.log(betterReverse("banana"))
+
+const obnoxiousReverse = string => string.length > 0 ? `${reverse(string.slice(1))}${string[0]}` : ""
+
+console.log(obnoxiousReverse("Truly obnoxious"))
