e59e17c8cc64a527c8ad01056da2766c56fff4b4
Bill Adams <vikingbill@gmail.com>
2022-09-14 12:33:45 -0400

    add lecture code, notes

--- w3/d3/lecture.js
+++ w3/d3/lecture.js
@@ -1,15 +1,22 @@
-let arr = [1, 1, 3, 2, 2, 1]; // establish array
+var colors  = [];
 
-const result = arr.reduce(function(acc, num) { // calling reduce, and supply anonymous function as arg, as well as 
-                                                // default acccumulator value
-  console.log("acc", acc)
-  console.log("num", num)
-  if (acc[num]) { // attempt to key into our acc at value, and if we get something truthy 
-    acc[num] = acc[num] + 1; // increment the associated value
-  } else {
-    acc[num] = 1; // else, establish that key with a value of 1
-  }
-   return acc; // return acc so it persists into next iteration
-}, {});
+var choice = (function(){
+    var colors = ["red", "orange","yellow","green","blue","indigo","violet"];
+    var color = colors[Math.floor(Math.random()*100) % colors.length];
+    console.log("the color is:", color);
+    var colors = ["This is a test!"];
+    return color;
+})()
 
-console.log(result)
+if (false){
+    console.log("Hello!")
+}
+
+// console.log("value of colors var: ", colors)
+// console.log("value of choice var: ", choice)
+// // console.log("Color var:", color)
+// function greet(){
+//     return "Hello!"
+// }
+// console.log(greet)
+// console.log(greet())
