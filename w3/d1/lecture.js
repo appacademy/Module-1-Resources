a1d8967908f521e79f6fe40bc0cecb22034d912f
Bill Adams <vikingbill@gmail.com>
2022-10-10 17:14:50 -0400

    w3d1

---
+++ w3/d1/lecture.js
@@ -0,0 +1,38 @@
+let greeting = "Hello"
+let firstName = "Biff"
+let lastName = "Cannon"
+
+// greeting += " "+ firstName + " " + lastName +"!"
+
+// console.log(greeting)
+// console.log(`${greeting} ${firstName} ${lastName}!`)
+
+let firstArr = ["Brandon", "Jojo","Jesse","Braxton","Charlie"]
+let lastArr = ["Laursen", "Yih", "Christensen", "Kappes", "Sheppard"]
+// debugger
+// console.log(`${firstArr.map((name, i) => `${name} ${lastArr[i]}` ).join(", ")} are the GOATS!`)
+
+
+// SHORT BREAK UNTIL ALL ROOMS FINISH!
+
+function foo(word){
+    debugger;
+    console.log(word)
+    bar("barber")
+    debugger
+}
+
+function bar(word){
+    debugger;
+    console.log(word)
+    baz("bazaar")()
+    debugger
+}
+
+function baz(word){
+    console.log(word)
+    debugger;
+}
+debugger;
+foo("food")
+debugger
