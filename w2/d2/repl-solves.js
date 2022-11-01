02b1defff790eba89330f94dc39f0f1714d64792
brandon <brandonlaursen95@yahoo.com>
2022-11-01 17:12:52 -0400

    added more repl solves

--- w2/d2/repl-solves.js
+++ w2/d2/repl-solves.js
@@ -79,3 +79,30 @@ const isPalindrome = seq => {
   }
   return Object.keys(map).length < 2;
 }
+
+
+
+//destructure array
+let bigArray = ["apple", 14, 32, 100, { name: "party" }, ["pineapple"]];
+let [first, second, third, fourth, fifth, sixth] = bigArray;
+console.log(second); // prints: 14
+console.log(fifth); // prints: { name: "party" }
+console.log(sixth); // prints: ["pineapple"]
+
+//restSum
+const restSum = (...otherNums) => {
+  let sum = 0;
+  for(let num of otherNums) {
+      sum += num;
+  }
+  return sum;
+}
+
+//spreadItOut
+const spreadItOut = (array1, array2) => {
+  return [...array1, ...array2];
+}
+//breakDownObj
+const breakDownObj = obj => {
+  return [...Object.keys(obj), ...Object.values(obj)]
+}
