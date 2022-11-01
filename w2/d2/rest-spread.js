801c577dbde3776f54db1d64735a1430d9e9aa75
brandon <brandonlaursen95@yahoo.com>
2022-11-01 13:19:47 -0400

    up to rest-spread done

---
+++ w2/d2/rest-spread.js
@@ -0,0 +1,12 @@
+/*
+
+  Rest and Spread Operator!
+
+  - The Rest operator allows us to accept any arbitary number
+    of arguments inside a function
+
+    - We know function can take fewer args than expected,
+      - param will be undefined if no args is passed
+    - javascript can also accept extra arguments
+      - using rest
+*/
