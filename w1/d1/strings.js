e7a665ff34831976e1378d3d3adae2234addf252
brandon <brandonlaursen95@yahoo.com>
2023-02-21 14:14:25 -0500

    partial string lecture

---
+++ w1/d1/strings.js
@@ -0,0 +1,32 @@
+/*
+
+
+  Strings!
+  - Strings delineated with "", '', ``
+
+*/
+
+
+// console.log('hello world');
+
+
+//strings can be stored in variable
+
+let newString = 'Hello';
+let newString2 = 'Hello';
+
+let sentence = 'you can have an entire sentence in a string'
+
+//what if i need to use quotes
+//alternate the quotes
+let sentence2 = '  "You\'re great!!" said Krandon!  '
+let sentence3 = " 'you\"re great' said krandon"
+
+//string methods!
+//all strings have a length property
+//tells us how many characters are in that string
+
+let name = 'brandon';
+
+console.log('brandon'.length)
+console.log(name.length);//7
