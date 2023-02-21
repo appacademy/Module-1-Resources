884d2f08b87e8f4f08a5e56fef57faa363abe906
brandon <brandonlaursen95@yahoo.com>
2023-02-21 13:16:05 -0500

    up to comparison operators

---
+++ w1/d1/variables.js
@@ -0,0 +1,81 @@
+
+
+/*
+
+
+  Variables!
+    - like storing a value in a box
+    - variables evaluate to the value stored in them
+    Values could be
+      - string
+      - numbers
+      - boolean
+      - function
+      - and more!
+
+      Example
+*/
+
+
+/*
+Variable initialization - both declaring and assignment
+Variable declaration - declaring the variable
+Variable assignment - assigning a value to a variable
+
+ = - is the assignment operator
+*/
+
+//camelCase
+//variables name descriptive
+// firstName, lastName
+let variableName; //variable declaration
+variableName = 'hello'//variable assignment
+
+
+//use that let keyword, follows by variable name, then assignment operator - =, then the value to assign to the variable
+let variableName2 = 'hello 2';//variable initialization
+
+
+//we can store many data types in a variable
+let firstName = 'brandon';
+let age = 27;
+let isSoftwareEngineer = true;
+
+// console.log(firstName);//brandon
+// console.log(age);//27
+// console.log(isSoftwareEngineer);//true
+
+//variable reassignment
+// console.log(age);
+//we can reassign the = assignment operator
+age = 300;//no let keyword
+// console.log(age)
+
+
+let num = 1;
+// // console.log(num);
+// num++;
+// console.log(num)
+// //num++ is shorthand for
+// num += 1;
+// console.log(num);
+// //num += 1 shorthand for
+// num = num + 1;
+// console.log(num);
+
+console.log(num + 'hey');//1hey
+/*
+  the plus operators doubles as addition but also concatenation
+  if we use the + operator with a string it will type case the other data type even if its not a string
+
+  KNOW YOUR DATA TYPES!
+*/
+
+// let number = '1'
+// console.log(typeof number)
+// console.log(typeof 1 === 'number');
+// console.log(typeof true === 'boolean')
+// console.log(typeof +number === 'number');//type cast this  string using the + into a number
+// console.log(typeof(1))
+
+
