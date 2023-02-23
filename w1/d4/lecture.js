a4fdce60efe77dca444a81999a0c00ef5b7b5a79
jojo <johnathanyih@gmail.com>
2023-02-23 09:15:10 -0800

    additional helper function

--- w1/d4/lecture.js
+++ w1/d4/lecture.js
@@ -53,8 +53,47 @@ let checkBalance = function () {
   console.log('Your current balance is: ')
 }
 
-console.log(transaction('deposit', 5)) // 15
-console.log(transaction('deposit', 50000)) // error
-console.log(transaction('withdraw', 10)) // 5
-console.log(transaction('withdraw', 10)) // error
-console.log(transaction('check balance'))
+// console.log(transaction('deposit', 5)) // 15
+// console.log(transaction('deposit', 50000)) // error
+// console.log(transaction('withdraw', 10)) // 5
+// console.log(transaction('withdraw', 10)) // error
+// console.log(transaction('check balance'))
+
+// **************************
+
+/**
+ * Polyas
+ * Understand the prob
+ * Make a plan
+ * Execute the plan
+ * Refactor
+ * 
+ * Goal: build a function that utilizes helper functions to solve some arithmetic.
+ * -- Given some input (string and two nums), perform the correct operation and return result
+ * -- Likely to if statements
+ *
+ * Valid operations:
+ * -- addition
+ * -- subtraction
+ * -- multiplication
+ * -- division
+ * 
+ * Bonus：add more than two numbers???
+ */
+
+/**
+ * Define overarching math function, called "calculator":
+ * - general structure: if -operation-, call that operation:
+ * -- e.g. if conditional === addition, then call addition helper
+ * 
+ * Create helper functions:
+ * addition:
+ * - add the two numbers
+ * subtraction:
+ * - subtract the two numbers
+ * multiplication:
+ * - multiply the two numbers
+ * division:
+ * - divide the two numbers
+ * Error message: if operation fails / doesn't return a number
+ */
