e36c8504d0978a3eb8287a57d491d5813c1082e4
jojo <johnathanyih@gmail.com>
2023-03-30 11:06:05 -0700

    hope we all get closure in our lives :)

--- w2/d4/lecture.js
+++ w2/d4/lecture.js
@@ -107,3 +107,111 @@ function myFunc() {
  * Where are the values before they are hoisted when using let and const?
  * Temporal dead zone! where variables exist before they are accessed
  */
+
+/**
+ * Read/Watch: 47 mins
+  - Refactoring Variables
+  - JavaScript Variables Quiz
+  - Declaring Variable Free Response
+  - Scope Debugging
+  - Scope Debugging Quiz
+  - Scope Quiz
+  - Take a break!
+ */
+
+/**
+ * CLOSURE
+ * what is closure?
+ *  - when a parent function cant access a data in the child function, BUT
+ *    child function can access data in the parent function
+ *  - when an inner function uses or changes variables in an outer function
+ *  - inner function has access to any variables within its own scope, AND
+ *    any variables in the outer function
+ * 
+ *  - a closure will keep reference to all its variables when its defined,
+ *    EVEN IF the outer function has returned
+ *  - inner function CLOSES OVER the outer function
+ * 
+ *  - the lexical environment consists of any and all variables within the scope in which the closure was declared (local inner scope, outer function scope, and global scope)
+ *  - anytime a func is declared, a lexical environment is, in essence, created and attached to the function
+ *  - this is how data is preserved
+
+ */
+
+/*
+our inner func has acces to the outer funcs variables
+*/
+function pizzaMaker(toppings) {
+  let order = "I'd like a pizza with ";
+
+  //inner func :O
+  function over(){
+    return order + toppings
+  }
+
+  return over();
+}
+// console.log(pizzaMaker('cheese'))
+
+/*
+our inner func are able to access AND MODIFY the variables of the outer func
+ */
+function groceryList(list) {
+  let groceries = list;
+
+  function addGrocery() {
+    groceries.push('and goldfish')
+  }
+
+  addGrocery()
+  return groceries
+}
+// console.log(groceryList(['turtle chips', 'thin mints']))
+
+/*
+access, modify, and RETURN A FUNC
+bc funcs are first class objects :)
+*/
+function elephantCollector() {
+  let elephants = ['dumbo']
+
+  return function(name) {
+    elephants.push(name)
+    return elephants
+  }
+}
+
+// let parade = elephantCollector();
+// console.log(parade) //anon func
+// console.log(parade('whiskers')) //array of dumbo and whiskers
+// console.log(parade('elenore')) //array of dumbo, whiskers, and elenore
+
+
+function createCounter(){
+  let count = 0;
+
+  return function() {
+    count++;
+    return count;
+  }
+}
+
+console.log(createCounter()()) //1
+console.log()
+
+console.log(createCounter()()) //1
+console.log()
+
+//creating a separate lexical environment
+let counter1 = createCounter()
+console.log(counter1()) //1
+console.log(counter1()) //2 because it refers to the same lexical environment tied to counter
+
+console.log()
+let counter2 = createCounter()
+console.log(counter2()) //1
+console.log(counter2()) //2
+console.log(counter2()) //3 because it refers to the same lexical environment tied to counter2
+
+console.log()
+console.log(counter1()) //3 bc it refers to lex.enviro. tied to counter1
