02534041c26d1ced6d917b276685fab0bb54f7fd
jojo <johnathanyih@gmail.com>
2023-04-04 11:12:09 -0700

    w3d1 update

---
+++ w3/d1/debugger-test.js
@@ -0,0 +1,13 @@
+function sum(array) {
+  let totalSum = 0;
+  debugger
+  for (let i = 0; i < array.length; i++) {
+    debugger
+    totalSum += array[i]
+  }
+
+  return totalSum
+}
+
+let arr = [1, 2, 3]
+sum(arr)
