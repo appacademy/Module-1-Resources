ff32350bde8477113b5ad70efbc5855bbdd34ac5
brandon <brandonlaursen95@yahoo.com>
2023-04-17 19:57:26 -0400

    add lecture notes

---
+++ w1/d1/variables.js
@@ -0,0 +1,34 @@
+/*
+
+
+  Variables!
+  - like storing a value in a box
+  - variables evaluate to the value stored in the box
+
+  Values could be:
+    - string
+    - numbers
+    - booleans
+    - arrays
+    - function
+*/
+
+/*
+  Variable initialization - both declaring and assignment
+  variable declaration - declaring the variable
+  variable assignment - assigning a value to a variable
+    we use the assignment operator -> =
+*/
+
+let variableName; //variable declaration -  weve declared the variableName variable
+variableName = 'hello'; //variable assignment - weve assigned the the variable, variableName the string 'hello' using the assignment operator -> =
+// console.log(variableName);
+variableName = 'goodbye';
+// console.log(variableName);
+
+//variable initialization
+//both the process of assignment and declaration
+let variableName1 = 'hello 2';
+console.log(variableName1);
+let name, age, town;
+name = 'brandon';
