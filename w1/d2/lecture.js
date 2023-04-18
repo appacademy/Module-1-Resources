dbbd27f7bd82696aa46863338e5180363eaed275
brandon <brandonlaursen95@yahoo.com>
2023-04-18 12:08:13 -0400

    comparison operators

---
+++ w1/d2/lecture.js
@@ -0,0 +1,35 @@
+/*
+
+
+  Comparison Operators
+
+    < - less than
+    > - greater than
+    == - loose equality
+    === - strict equality
+    <= - less than equal too
+    >= - greater than equal too
+
+    They return a boolean - true or false;
+*/
+
+// console.log(2 < 3);//t
+// console.log(10 > 5);//t
+// console.log(10 === 10);//t - check the value and the data type to make sure they are the same
+// console.log(10 == '10');//t - check the value, but not the data type
+// console.log(10 === '10');//f - check the value and the data type, the value is 10, but the data types are different, string vs number
+
+//strictly equals
+//when we use triple equals ===
+//we are comparing the value and the data type
+
+//lose equals
+// double equals - ==
+//we are comparing just the value not the data type!
+
+//0 1 2 3 4 5 6
+//a b c d e f
+
+//we can compare letter
+console.log('a' > 'b');//false
+// https://www.asciitable.com/
