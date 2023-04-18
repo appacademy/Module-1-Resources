e94fccd68ab6d1b29714ac16219da31456151e2a
brandon <brandonlaursen95@yahoo.com>
2023-04-18 14:21:44 -0400

    more clarfying nores

--- w1/d2/functions.js
+++ w1/d2/functions.js
@@ -22,6 +22,12 @@
   - verb followed by a noun, CRUD getInfo, deleteInfo, editInfo
 */
 
+
+//function declaration
+//function keyword
+//followed by name, usually camelCase - functionName
+//parenthesis, where you takin in parameters
+//curly braces - are used to signify a code block
 //example -  with a function that is never called
 //nothing happens, this function is never called!
 function functionName() {
@@ -45,9 +51,10 @@ function functionName2() {
 //we call the function, we log the console.log, because there is no return, the return value defaults to undefined
 
 //were saying print to the terminal the return value of calling this function
-// console.log(functionName2())
+// console.log(functionName2());//there is not return value -  therefore print undefined
 
 //example - with a function that does return and console.log
+//this example will both console.log the return value and call the console.log from within in the function
 function functionName3() {
   console.log('hey from functionName 3');
   return 'hey from functionName 3'
@@ -57,13 +64,20 @@ function functionName3() {
 // console.log(functionName3());//hey from functionName 3
 // functionName3();
 //some things to keep in mind
-//you will only see the return value of a function if you wrap your function invocation in a console.log
+//you will only see the return value of a function if you wrap your function invocation in a console.log ex: console.log(functionName())
+
 //the function will still run if its called, but you wont see anything unless you console.log
 
 //console.log to see
 //return is what your function evaluates to
 
+
+//arguments and parameters
+//parameters are the variables used to represent the arguments passed
+//arguments are passed to the function
+
 //name variable, is a parameter to represent the argument passed
+//doesnt have to be called name, could be anything
 function printName(name) {
   console.log('1st',name);
   return name;//this is what your function will evaluate too
@@ -73,4 +87,3 @@ function printName(name) {
 console.log('2nd', printName('brandon'));//pass the string brandon as a argument to the function printName
 
 printName(true, 1, 2)
-console.log()
