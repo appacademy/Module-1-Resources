daf8711de8e3fb7e95113e4bf05178eee3a90e53
brandon <brandonlaursen95@yahoo.com>
2023-04-20 13:16:25 -0400

    lecture done

--- w1/d4/lecture.js
+++ w1/d4/lecture.js
@@ -87,8 +87,90 @@ let transaction = function(type, amount) {
   return 'Balance ' + balance;
 };
 
-console.log(transaction('deposit', 1000));
-console.log(transaction('withdraw', 10));
-console.log(transaction('check balance'))
+// console.log(transaction('deposit', 1000));
+// console.log(transaction('withdraw', 10));
+// console.log(transaction('check balance'))
 
 // Goal: build a function that utilizes helper functions to solve some math problems.
+
+//take a break!!!
+//and interpret this code
+//type out your own code!
+//make sure to git pull!
+
+
+//paramaters from func to func
+
+/*
+      Polyas
+      Understand the problem
+      Goal: Build a function that utilize helper function to solve some math problems!
+        - Input: a string(operation) and two numbers    => addition 10 3
+        - Output: the result of calling the operation using the two numbers passed => 13
+        * will likely use if statements
+        * upon meeting a condition, we want to invoke the proper function according to the operation
+        * Valid operations
+        *   - addition
+        *   - subtraction
+        *   - multiplication
+        *   - division
+        *
+
+      Make a Plan
+        1. define a function that takes in a string and two nums
+        2. conditionally check what are string is, this determine the operator
+          a. depending on the operator
+            -if its addition
+              - call a function that adds two numbers
+            -if its subtract
+              - call a func that subtracts two numbers
+            -if its multiplication
+              - call a func that multiplies two numbers
+            -if its division
+              - call a func that divides two numbers
+            -else console.log something went wrong?
+
+
+      Refactor
+
+*/
+
+//Execute the plan
+
+//addition function
+let add = function(fruit1, fruit2) {
+  return fruit1 + fruit2;
+};
+
+//subtraction function
+let subtract = function(num1, num2) {
+  return num1 - num2;
+};
+
+// * helper functions
+let multiplication = function(num1, num2) {
+  return num1 * num2;
+}
+// / helper func
+let division = function(num1, num2) {
+  return num1 / num2;
+}
+//10
+//let num1 = 10
+//let fruit1 =
+//main function
+let calculator = function(type, num1, num2) {
+  if(type === 'addition') {
+    return add(num1, num2);
+  } else if(type === 'subtraction') {
+    return subtract(num1, num2);
+  } else if(type === 'multiplication') {
+    return multiplication(num1, num2);
+  } else if(type === 'division') {
+    return division(num1, num2);
+  } else {
+    return 'not a valid operation'
+  }
+};
+
+console.log(calculator('addition', 10, 13));
