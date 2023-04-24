ca727787f33214ce3b034d80953ebe2da635c2ca
brandon <brandonlaursen95@yahoo.com>
2023-04-24 16:07:55 -0400

    fat arrow function

--- w2/d1/lecture.js
+++ w2/d1/lecture.js
@@ -34,10 +34,9 @@ let sayHello3 = (name) => "hello " + name;
 // console.log(sayHello3('brandon'));
 
 //with implicit return
-//if you have two args, you cannot omit the parathesis around the parameters
+//if you have two args, you cannot omit the parenthesis around the parameters
 let addNums = (num1, num2) => num1 + num2;
 // console.log(addNums(10, 13));//23
-
 
 //cant implicit return this
 let addNums1 = (num1, num2) => {
@@ -45,10 +44,18 @@ let addNums1 = (num1, num2) => {
   return res;
 };
 
-console.log(addNums1(10, 12))
+console.log(addNums1(10, 12));
+
 //function declaration vs expression/fat arrow
 //where you can access the function
 //function expression - ReferenceError: Cannot access 'addNums1' before initialization
+//cannot access function if function call is above the function
 //function declaration - can be accessed before initialization
+//can access function if function call is above, due to hoisting which we learn in week 3
 
-//
+//return an object implicitly
+// let obj = {}
+// a codeblock - {};
+//youll use thr grouping operator to groupe the object, so wrap object like so -> ( { key: value } )
+let returnObj = () => ({name: "brandon"});
+// console.log(returnObj());//{name: "brandon"}
