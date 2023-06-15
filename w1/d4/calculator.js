03df90c9b4f2290aad9c9525a84070090929dc75
brandon <brandonlaursen95@yahoo.com>
2023-06-15 13:01:40 -0400

    calc prompt

---
+++ w1/d4/calculator.js
@@ -0,0 +1,25 @@
+/*
+
+
+  Polya's
+  Goal: Build a function that utilize helper functions to solve math problems
+  * Understand the problem
+    - Input:  a string(operation) and two numbers => 'addition' , 10, 12
+    - Output: the result of calling the operation on the two numbers => 22
+    * will likely use if statements
+    * upon meeting a condition; invoke the proper helper function according the operation
+    * Valid operation
+    *   Addition
+    *   Subtraction
+    *   Multiplication
+    *   Division
+
+  * Make a plan
+  * Execute the plan
+  * Refactor
+
+*/
+
+// take 15min to create this calculator
+// console.log everything!
+// the main function will be the calculator
