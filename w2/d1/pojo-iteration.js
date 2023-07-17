737dbdb58a5d873b15acd5b55052c17ffa47b8f9
brandon <brandonlaursen95@yahoo.com>
2023-07-17 19:00:47 -0400

    pushing iterating with objects lecture notes

--- w2/d1/pojo-iteration.js
+++ w2/d1/pojo-iteration.js
@@ -24,7 +24,7 @@ let brandon = {
 // Looping through arrays
 
 let bandsArr = brandon.favoriteBands;
-console.log(bandsArr); //[ 'Joyce Manor', 'Neck Deep', 'Idles' ]
+// console.log(bandsArr); //[ 'Joyce Manor', 'Neck Deep', 'Idles' ]
 
 for(let i = 0; i < bandsArr.length; i++) {
   // console.log(bandsArr[i]);
@@ -70,9 +70,44 @@ let show = {
 
 // were gonna use the for in loop on the show object
 // well get all the keys, and be able to use tho keys to get the values
+
 for(let key in show) {
-  console.log('key', key);
-  console.log('value', show[key]);
+  // console.log('key', key);
+  // console.log('value', show[key]);
 }
 
-// Dont use for in on arrays/strings
+// can name variable after let anything, its bananable
+// for(let banana in show) {
+//   console.log('banana ', banana);
+//   console.log('value', show[banana]);
+// }
+
+
+// ! Dont use for in on arrays/strings
+
+// Other ways to get keys and values
+// built in methods
+
+// get all the keys
+// returns an array of keys
+let keysArr = Object.keys(show);
+// console.log(keysArr); //[ 'name', 'series', 'captains' ]
+
+// now that we have an array of keys
+// we can iterate through this array to get the values
+for(let i = 0; i < keysArr.length; i++){
+  let key = keysArr[i];
+  // console.log('key', key);
+  // console.log('value', show[key]);
+}
+
+
+// get all the values
+let valuesArr = Object.values(show);
+// console.log(valuesArr);
+
+// get all entries
+// returns a matrix with nested arrays, with the key as the first index
+// the value as the second index
+let entryArr = Object.entries(show);
+console.log(entryArr);
