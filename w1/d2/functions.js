edca1409c36f8de41f5b7489bef30e941ccd380a
brandon <brandonlaursen95@yahoo.com>
2023-08-08 14:13:39 -0400

    starting functions

---
+++ w1/d2/functions.js
@@ -0,0 +1,39 @@
+/*
+
+
+  Functions
+    * A function is a procedure of code that will when its called
+    * Writing a function is know as a function declaration or function definition
+    * create reusable logic - do not have repeat writing the same lines of code
+    * Logic to be ran to output a value
+
+    * JavaScript reads top to bottom
+    * Function is not read till its called/invoked
+
+  So three things to remember about return statements are:
+    * Every function call evaluates to its return value
+    * Every function is js returns undefined unless a return is specified
+    * Once a return statement is encountered, the function will immediately stop and return the value; anything below the return is ignored
+
+  Function naming convention
+    * camelCase => myFunc
+    * descriptive names
+    * verb followed by a noun: CRUD - getInfo, deleteInfo, editMessage
+    * If the return value is a boolean; you could name is checkBoolean
+
+  Declaring/Defining a function
+    * where parameters are used to represent the argument passed
+    function functionName(param1, param2) {
+      return undefined;
+    }
+
+  Calling/invoking a function
+    * Where we pass the arguments the function
+    * A function will not run unless its called/invoked
+    * A function call evaluates to return value
+    functionName(arg1, arg2);
+
+  If you want to see what a function evaluates too; its return value.
+    * You would wrap the function call/invocation in a console.log
+    console.log(functionName())
+*/
