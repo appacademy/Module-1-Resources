6b8a09faaece24ca5fd814f4f72e51d4fdb76bc7
brandon <brandonlaursen95@yahoo.com>
2023-08-08 11:46:08 -0400

    notes up to variables

---
+++ w1/d2/variables.js
@@ -0,0 +1,32 @@
+/*
+
+
+
+  Variables!
+    * A variables named reference to value
+    * That way an unpredictable value can be accessed through a predetermined name
+    * Can be used to store the result of an expression
+    * Think of like an item in a box
+
+  Values that variables could hold could be:
+    * string
+    * numbers
+    * booleans
+    * function
+    * expressions
+    * and more!
+
+  Variable Naming guide
+    * https://developer.mozilla.org/en-US/docs/MDN/Writing_guidelines/Writing_style_guide/Code_style_guide/JavaScript
+
+  Variable initialization
+    * Both declaring and assignment
+  Variable declaration
+    * declaring the variable name
+  Variable assignment
+    * assigning a value to a variable
+  
+  - = is the assignment operator
+    * used to assign and save a value to variable
+
+*/
