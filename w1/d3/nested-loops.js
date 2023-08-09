1f51081a6af9201e7966e4ead9d2979d9b1259f5
brandon <brandonlaursen95@yahoo.com>
2023-08-09 15:52:42 -0400

    nested loops

--- w1/d3/nested-loops.js
+++ w1/d3/nested-loops.js
@@ -6,11 +6,9 @@
 
 */
 
+let array = ["a", "b", "c"];
 
-
-let array = ['a','b','c'];
-
-for(let i = 0; i < array.length; i++){
+for (let i = 0; i < array.length; i++) {
   let letter = array[i];
   // console.log('index: ', i);
   // console.log('value at that index: ', array[i]);
@@ -33,4 +31,44 @@ for(let i = 0; i < 3; i++){
   }
 }
 
+let matrix = [
+  ["a", "b", "c"],
+  ["d", "e", "f"],
+  ["g", "h", "i"]
+];
 
+// console.table(matrix);
+// console.table(matrix[0])
+/*
+[ [ 'a', 'b', 'c' ], [ 'd', 'e', 'f' ], ["g", "h", "i"] ]
+*/
+// console.log(matrix);//
+
+
+console.log(matrix.length);// 3
+console.log(matrix[0]);// ["a", "b", "c"]
+console.log(matrix[1]);// [ 'd', 'e', 'f' ]
+console.log(matrix[2]);// [ 'g', 'h', 'i' ]
+
+console.log(matrix[0]);//[ 'a', 'b', 'c' ]
+console.log(matrix[0][0])// a
+console.log(matrix[0][1]);// b
+console.log(matrix[0][2]);// c
+
+console.log(matrix[1][0]);// d
+console.log(matrix[1][1]);// b
+console.log(matrix[1][2]);// b
+
+
+
+for(let i = 0; i < matrix.length; i++) {
+  // console.log('i: ', i, matrix[i]);
+  let subArr = matrix[i];
+  console.log('subArr: ',subArr);
+
+  for(let j = 0; j < subArr.length; j++){
+    console.log('j: ', j, 'value: ', subArr[j]);
+    // console.log(matrix[i][j] == subArr[j]);
+  }
+  
+}
