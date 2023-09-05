a8846651fc4a3e805e7881e0467d08cde3071e79
brandon <brandonlaursen95@yahoo.com>
2023-09-05 19:57:22 -0400

    end of lecture

--- w1/d1/lecture.js
+++ w1/d1/lecture.js
@@ -1,1 +1,75 @@
-console.log('welcome to app academy')
+/*
+
+
+
+  Intro to JavaScript
+
+  What is JavaScript?
+    * JavaScript is a programming language that was originally developed to increase user interactivity with websites, and has grown to become a useful programming language in the backend of applications as well. JavaScript is now one of the most popular and widely used programming languages for building web applications today.
+
+  Why is JavaScript so useful?
+    * Dynamic web design
+    * asynchronous
+    * enables more responsive website
+    * integrate with two core languages
+      * html and css
+
+  What is debugging?
+    * Fixing errors in your logic or code
+    * Find problems and fix them
+
+  How we do we debug?
+    * console.log
+    * run code to find errors
+    * unit test
+    * evaluate what the problem is
+    * syntax error
+    * devtools
+    * mocha
+    * go line by line
+    * debugger
+    * trial and error
+    * error messages
+
+
+  Styling Guide - https://developer.mozilla.org/en-US/docs/MDN/Writing_guidelines/Writing_style_guide/Code_style_guide/JavaScript
+  JS Semi-Colon - Insertion - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#automatic_semicolon_insertion
+
+  Lets write some code!
+
+*/
+
+
+// single line comment
+// double back slash - //
+
+/*
+this
+is
+a
+multiline
+comment
+delineated by: - // forward slash + asterisk *
+*/
+
+
+// Comments
+// * use comments to explain what your code is doing
+// * create a plan using polya's
+// * notes
+// * mac - command + /
+// * win - control + /
+
+
+// we use console.og whenever you want to see something on the screen
+console.log('hello world');
+
+
+// preview on numbers
+// * can be used to evaluate expression
+// * an expression is anything that results in a value
+// * a console.log, will first evaluate the expression than prints its value
+// * expression: must first be evaluated to a single value
+// * value: data type: number, string, object, array
+
+console.log(6 + 5);
