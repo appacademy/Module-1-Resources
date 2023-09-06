8323c5b59919d832fa4914058ec9d37f9afe1f33
brandon <brandonlaursen95@yahoo.com>
2023-09-06 11:47:10 -0400

    end of numbers

---
+++ w1/d2/lecture.js
@@ -0,0 +1,16 @@
+
+
+console.log(1 + 2 + 19);// 22
+
+// preview on variables
+let name = 'brandon';
+
+console.log(name);
+console.log('brandon');
+// * variables evaluates to what it holds
+// * variables are just expression that are waiting to be evaluated to get the value
+
+
+// console.log can take in additional arguments
+console.log('hey look this here is a name ===>', name);
+
