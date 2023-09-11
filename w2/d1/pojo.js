d48783b3467bf1129088f6a93fbdb0270e8759a6
brandon <brandonlaursen95@yahoo.com>
2023-09-11 16:00:06 -0400

    partial object;

--- w2/d1/pojo.js
+++ w2/d1/pojo.js
@@ -63,4 +63,61 @@ let brandon = {
   },
 };
 
+// lets access some properties on the brandon object
+// * in order to access they value, we will need the key
+// * can use either bracket or dot notation
 
+// accessing name key
+console.log(brandon);
+console.log(brandon.name);// brandon
+console.log(brandon['name'])// brandon
+
+
+// access age key
+console.log(brandon.age);// 27
+console.log(brandon['age']);// 27
+
+
+// access favorite bands key
+console.log(brandon.favBands);// [ 'neck deep', 'idles', 'joyce manor' ]
+console.log(brandon['favBands']);// [ 'neck deep', 'idles', 'joyce manor' ]
+// * can index into array gain access to it
+console.log(brandon['favBands'][0]);// neck deep
+
+
+// access location key
+console.log(brandon.location);//{ city: 'Tampa', state: 'Florida' }
+console.log(brandon.location.city);//Tampa
+console.log(brandon['location']['city']);//Tampa
+
+// mix and match the way index
+console.log(brandon.location['city']);// Tampa
+console.log(brandon['location'].city);// Tampa
+
+/*
+
+  Accessing a method
+    * A method is just a function that belongs to an object
+    * the sayHello function belongs to the brandon object
+    * the key is sayHello
+    * the value is the function
+
+  We can access the function just like any other key:value pair
+
+*/
+
+console.log(brandon.sayHello);// [Function: sayHello]
+
+console.log(brandon.sayHello());// Hello undefined
+
+console.log(brandon.sayHello('maica'));// Hello maica
+
+console.log(brandon['sayHello']('charles'))// Hello charles
+
+
+// * we can use bracket notation on methods weve been using
+let numbers = [1,2,3];
+numbers.push(4);
+numbers['push'](5);
+// console.log(numbers);// [ 1, 2, 3, 4, 5 ]
+// console['log'](numbers);//[ 1, 2, 3, 4, 5 ]
