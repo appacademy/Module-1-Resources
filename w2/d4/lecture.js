eccd3ce6595facae795c3356f505d3aa8a045aff
brandon <brandonlaursen95@yahoo.com>
2023-09-14 12:20:38 -0400

    testing let

--- w2/d4/lecture.js
+++ w2/d4/lecture.js
@@ -135,3 +135,93 @@ function myFunc3(){
 
 
 */
+
+
+// * review
+let variable1; // variable declaration
+variable1 = 'string';// variable assignment
+let variable2 = 'string';// variable initialization
+
+/*
+  var
+  * function scoped
+  * can be reassigned
+  * can be re declared
+  ! error prone
+
+  * when var is hoisted
+    ** only the the variable declaration is hoisted
+    ** when var is hoisted, it has a default value of undefined
+    ** therefore when we hoist var, we do not get an error because we get undefined
+*/
+
+
+// * hosting var
+// ! no errors with var
+function testingVar(){
+  // * hoist var
+  // ! no error because default value is undefined
+  // console.log(test);// undefined
+  var test = 1;
+
+  // * var can be reassigned
+  test = 2;
+  console.log(test);// 2
+
+  // * var can be re declared
+  var test = 3;
+  console.log(test);// 3
+
+  // ! since var is function scoped
+  // * we can access var declared variables from an outer scope
+  if(true){
+    var access = true;
+  }
+  // * var is function scope
+  // console.log(access);//true
+}
+
+
+// testingVar();
+
+
+/*
+
+  let
+    * block scoped
+    * can be reassigned
+    * cannot re declared
+
+  * when let is hoisted
+    ** let does not have a default value
+    ** therefore will throw an error when hoisted
+    ** as the variable does yet exists
+
+*/
+
+
+function testingLet(){
+  // * hoisting let
+  // ! error
+  // console.log(test);// ReferenceError: Cannot access 'test' before initialization
+  let test = 1;
+
+  // * let can be reassigned
+  test = 2;
+  console.log(test);// 2
+
+  // * let cannot be re declared
+  // let test = 3;
+  //SyntaxError: Identifier 'test' has already been declared
+
+  // * let is block scoped
+  // ! we cannot access inner block from outer
+  if(true){
+    let access = false;
+  };
+
+  // let is block scoped
+  // console.log(access);// ReferenceError: access is not defined
+};
+
+testingLet();
