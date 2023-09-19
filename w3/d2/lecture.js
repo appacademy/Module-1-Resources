22cff69157101a8ae9fa3314aa1f96df241c3b16
brandon <brandonlaursen95@yahoo.com>
2023-09-19 11:17:47 -0400

    callstack notes

---
+++ w3/d2/lecture.js
@@ -0,0 +1,15 @@
+/*
+
+
+
+  Call Stack
+    * The call stack is a structure that js uses to keep track evaluation of function calls
+    * The function is at the top of the stack; is the function that js is currently being executed
+    * stack data structure
+      * Ex: stack of plates
+    * first in last out/ last in first out
+    * When a function is CALLED/INVOKED it pushed on to the stack
+    * When a function is returned, it is popped off the stack
+      * If there is no return keyword; if there is no code let to run then the function defaults undefined
+
+*/
