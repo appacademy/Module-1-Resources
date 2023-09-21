ae59be4f04169f8c9378dee53e6cc6e980b9550c
brandon <brandonlaursen95@yahoo.com>
2023-09-21 12:16:08 -0400

    fib and event loop

--- w3/d4/lecture.js
+++ w3/d4/lecture.js
@@ -85,12 +85,95 @@ function synchronous() {
 // this does not block the execution of the call stack
 // *  because async code is sent to the message queue
 
-setTimeout(() => {
-  console.log("first");
-}, 5000);
+// setTimeout(() => {
+//   console.log("first")
+// }, 0);
 
-setTimeout(() => {
-  console.log('second');
-},0);
+// setTimeout(() => {
+//   console.log('second');
+// }, 2000);
 
-console.log('third');
+// console.log('third');
+
+/*
+
+  setTimeout
+
+  What is a setTimeout?
+    * The global setTimeout() method sets a timer which executes a function or specified piece of code once the timer expires.
+    * setTimeout(callbackFunction, delayInMilliseconds, param1, param2);
+    * you can pass anon callback to a setTimeout
+
+  What does a setTimeout return?
+    * The returned timeoutID is a positive integer value which identifies the timer created by the call to setTimeout(). This value can be passed to clearTimeout() to cancel the timeout
+
+*/
+
+// console.log(global);
+
+// setTimeout(console.log, 2000, 'hello');
+
+function time() {
+  console.log("time is up");
+}
+
+// setTimeout(time, 5000)
+
+function sayHi(name1, name2) {
+  console.log(`hello ${name1} and ${name2}`);
+}
+
+// setTimeout(sayHi, 2000, "brandon", "maica");
+
+// setTimeout(
+//   (name1, name2) => console.log(`hello ${name1} and ${name2}`),
+//   2000,
+//   "brandon",
+//   "maica"
+// );
+
+// clear timeout
+// *  this will prevent the function from running at all
+
+// let timer = setTimeout(
+//   (name) => {
+//     console.log("hello " + name);
+//   },
+//   1000,
+//   "charles"
+// );
+
+// the timer variable holds the time object
+// console.log(timer);
+// this prevents the setTimeout for running
+// clearTimeout(timer);
+
+
+
+
+// fib
+function somethingSlow(n) {
+  if (n === 1 || n === 2) return 1;
+
+  return somethingSlow(n - 1) + somethingSlow(n - 2);
+}
+
+function foo() {
+  console.log("food");
+}
+
+function bar() {
+  console.log("bark");
+  baz();
+}
+
+function baz() {
+  console.log("bazaar");
+}
+
+
+setTimeout(foo, 1500);
+setTimeout(bar, 1000);
+// * tree recursion
+console.log(somethingSlow(44));
+// Predict the output of the code????
