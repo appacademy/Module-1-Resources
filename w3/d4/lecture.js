960343d2081f57a35b050663d983f3520f7c301e
brandon <brandonlaursen95@yahoo.com>
2023-09-21 11:33:24 -0400

    event loop notes

--- w3/d4/lecture.js
+++ w3/d4/lecture.js
@@ -23,12 +23,74 @@
 
 // Synchronous code
 
-function synchronous(){
-  console.log('two');
-};
+function synchronous() {
+  console.log("two");
+}
 
-console.log('one');
-synchronous();
-console.log('three');
+// console.log('one');
+// synchronous();
+// console.log('three');
+
 // one => two => three
 
+/*
+
+
+  What is asynchronous code?
+    * no guaranteed order of execution
+    * does NOT block the execution of the call stack
+
+  Why do we need async code?
+    * perform two things at once
+    * account for user input
+     * users have short attention span
+    * Prevents websites from being non responsive
+    * have to send an api call
+      * retrieving user information
+    * more dynamic websites
+    * the environment in which we run out application is full of uncertainty
+
+  What is the Event Loop?
+    * Consists of the call stack and the message queue
+    * Checking if the call stack is empty, if it is we can move task from the message queue to the call stack
+
+  Call stack - the track the current task in progress
+    * Synchronous task
+    * Last in first out - first in last out
+       * ex: stack of plates
+    * Add to stack - pushing
+    * Removing from the stack - popping
+    * Stack structure
+
+  Message Queue - keep tracks of task that cannot be executed at the moment
+    * Asynchronous task
+    * First in first out - last in last out
+      * ex: standing in line
+    * add to back - enqueue
+    * remove from the front - dequeue
+    * queue structure
+
+    * Task can only be performed one at a time because?
+    * JS is a single threaded language!
+
+    NodeApi/Web api -  Built using C, interacting with API
+      * DOM
+      * ajax
+      * setTimeout
+
+*/
+
+// console.log("first");
+
+// this does not block the execution of the call stack
+// *  because async code is sent to the message queue
+
+setTimeout(() => {
+  console.log("first");
+}, 5000);
+
+setTimeout(() => {
+  console.log('second');
+},0);
+
+console.log('third');
