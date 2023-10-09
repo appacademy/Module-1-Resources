c12267139409d77b20b93c210b44473a7305aba6
brandon <brandonlaursen95@yahoo.com>
2023-10-09 13:50:50 -0400

    fat arrow notes

--- w2/d1/lecture.js
+++ w2/d1/lecture.js
@@ -5,16 +5,18 @@
     * More convenient way to pass callback functions(We will learn about on Wednesday)
     * Arrow functions are the preferred syntax when using an anonymous function as a callback.
       ** [1,2,3].map(num => num * 2);// [2,4,6]
+         [1,2,3].map(function(num){
+              return num * 2
+           });
 
 */
-
 
 // Function expression
 // * store an anon function in a variable
 
-let hello = function(name) {
-  return 'Hello ' + name;
-}
+let hello = function (name) {
+  return "Hello " + name;
+};
 
 // console.log(hello('brandon'));
 
@@ -29,8 +31,8 @@ let hello = function(name) {
 
 // Fat arrow function
 let arrowHello = (name) => {
-  let res = 'Hello ' + name;// statement 1
-  return res;// statement 2
+  let res = "Hello " + name; // statement 1
+  return res; // statement 2
 };
 
 // console.log(arrowHello('Maica'))
@@ -45,17 +47,18 @@ let arrowHello = (name) => {
   * Omit the curlys and the return keyword
 */
 
-
-let implicitHello = (name) => 'Hello ' + name;// statement 1
+let implicitHello = (name) => "Hello " + name; // statement 1
 // console.log(implicitHello('Charles'));
 
-let addition = (num1, num2) => num1 + num2;;
+let addition = (num1, num2) => num1 + num2;
 
 // console.log(addition(10, 20));//
 
 // Return an object using an implicit return
 // let obj = { name: 'brandon'};
 
-let returnObj = (name) => ({ firstName: name })
-
-console.log(returnObj('Trevor'));// { firstName: 'Trevor' }
+let returnObj = (name) => ({ firstName: name });
+// console.log(returnObj('Trevor'));// { firstName: 'Trevor' }
+function returnObj2(name) {
+  return { firstName: name };
+}
