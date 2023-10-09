d5565a267a59ea68e777cdfe3f2aac0afd615dbb
brandon <brandonlaursen95@yahoo.com>
2023-10-09 14:07:40 -0400

    pojo notes

--- w2/d1/pojo.js
+++ w2/d1/pojo.js
@@ -44,3 +44,45 @@
 */
 
 
+// create our own object
+// object literal - {}
+// key: value,
+let brandon = {
+  name: 'brandon',
+  age: 27,
+  favoriteBands: ["Joyce Manor", "Neck Deep", "Idles"],
+  location: { city: "Tampa", state: "Florida" },
+  sayHello: function(name) {
+    return 'Hello ' + name;
+  }
+}
+
+
+// lets access some properties of the brandon object
+// * in order to access the key, we will need the value
+// * can use either bracket or dot notation
+
+// * accessing name key
+console.log(brandon.name); // 'brandon'
+console.log(brandon["name"]); // 'brandon'
+
+// * accessing age key
+console.log(brandon.age); // 27
+console.log(brandon["age"]); // 27
+
+// * accessing favorite bands key
+console.log(brandon.favoriteBands); // ["Joyce Manor", "Neck Deep", "Idles"]
+console.log(brandon["favoriteBands"]); // ["Joyce Manor", "Neck Deep", "Idles"]
+
+// * accessing location key
+console.log(brandon.location); // { city: "Tampa", state: "Florida" }
+console.log(brandon["location"]); // { city: "Tampa", state: "Florida" }
+
+// * accessing nested locations key
+console.log(brandon.location); // { city: "Tampa", state: "Florida" }
+
+// * can mix and match notations
+console.log(brandon.location.city); // Tampa
+console.log(brandon["location"]["city"]); // Tampa
+console.log(brandon.location["state"]); // Florida
+console.log(brandon["location"].state); // Florida
