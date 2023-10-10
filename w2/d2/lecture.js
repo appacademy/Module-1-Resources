dff38b562e2079b2add223f0f1091d95bc07dc9c
brandon <brandonlaursen95@yahoo.com>
2023-10-10 11:31:49 -0400

    reference data types

--- w2/d2/lecture.js
+++ w2/d2/lecture.js
@@ -31,7 +31,7 @@ let newAge = age;
 
 age += 1;
 
-console.log('age:', age, 'newAge:', newAge);
+// console.log('age:', age, 'newAge:', newAge);
 // * Notice that the age has changed and newAge remains the same?!
 // * This is because primitive data types are immutable
 // * primitive data types have assigned spots in memory
@@ -41,3 +41,38 @@ console.log('age:', age, 'newAge:', newAge);
 // while age has been reassigned to point to 28
 // age -> 28
 // newAge -> 27
+
+
+// Reference data types
+// JS create references data types by assigning portions of memory
+// Dynamic memory allocation - memory needs to be freed and allocated for reference data types
+// Memory is allocated as needed in order to save memory
+
+// lets create a reference in memory
+// cat1 => <reference1>
+let cat1 = {
+  name: 'pumpkin',
+  age: 3
+};
+
+let cat3 = {
+  name: 'pumpkin',
+  age: 3
+};
+
+console.log(cat1 === cat3);// false
+// cat1 => <reference1>
+// cat3 => <reference3>
+
+let cat2 = cat1;
+// were pointing another variable at the same reference in memory
+// cat1 => <reference1>
+// cat2 => <reference1>
+
+cat1.name = 'celebi';
+// changing one changes both
+// both variables point the same references
+// <reference1> holds the properties
+
+console.log('cat1:', cat1, 'cat2:', cat2);
+// cat1: { name: 'celebi', age: 3 } cat2: { name: 'celebi', age: 3 }
