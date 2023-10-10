a6bd2098c82bbdf0518fa893ca6f3dcd5ef2bdb2
brandon <brandonlaursen95@yahoo.com>
2023-10-10 15:46:17 -0400

    done with destrfutring

--- w2/d2/rest.js
+++ w2/d2/rest.js
@@ -63,7 +63,7 @@ let numbers = [1,2,3,4,5,6];
 // use both rest and spread with destructing
 
 let [firstNum, ...rest] = [...numbers, 4];
-console.log(firstNum, rest);// 1 [ 2, 3, 4, 5, 6, 4 ]
+// console.log(firstNum, rest);// 1 [ 2, 3, 4, 5, 6, 4 ]
 // * to the right of the equal sign - spread into an new array literal all the numbers from the numbres array, but also add the number 4
 // -> [1,2,3,4,5,6,4];
 // * to the left of the equal sign, destruct positionally into the firstNum variable the first element in the array to the right of the equal sign and store the rest of the numbers in a variable called rest
@@ -80,6 +80,53 @@ let [person1, ...people] = [...names, 'caleb'];
 // person1 = 'brandon';
 // people = ['maica','trevor', 'charles', 'caleb']
 
-console.log(person1, people);
+// console.log(person1, people);
 //brandon maica trevor charles
 // brandon [ 'maica', 'trevor', 'charles', 'caleb' ]
+
+
+
+// ! Destructuring with Objects
+
+let user = {
+  name: "John",
+  address: {
+    street: "123 Street Rd",
+    city: "Orlando",
+  },
+  age: 30,
+};
+
+
+// Destructure some values out of an object by providing the key
+// we want to destruct out the name
+
+// let { name, address, address: { city } } = user;
+// console.log(name, city, address);// John Orlando
+
+
+// use rest!
+let { name, ...restOfObj} = user;
+
+// console.log(name, restOfObj);
+// John { address: { street: '123 Street Rd', city: 'Orlando' }, age: 30 }
+
+
+// destructure in a loop
+
+let entries = Object.entries(user);
+for(let [key, value] of Object.entries(user)){
+  // console.log(entry);
+  // console.log(key, value);
+  // let key = entry[0];
+  // let value = entry[1];
+};
+
+function destructure({ name }){
+  // console.log(prop);// { name: 'brandon' }
+  // let { name } = prop;
+  console.log(name);
+}
+
+
+console.log(destructure({name: 'brandon'}))
