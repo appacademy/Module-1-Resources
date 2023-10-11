23e8a3894054e8f2f3cc317e19a8c42de9fe1245
brandon <brandonlaursen95@yahoo.com>
2023-10-11 12:03:07 -0400

    callback notes

--- w2/d3/lecture.js
+++ w2/d3/lecture.js
@@ -118,3 +118,42 @@ let double = (number) => number * 2;
 //     return number * 2;
 //   })
 // );
+
+function add(num1, num2, cb) {
+  // console.log(num1, num2, cb);
+  if (cb === undefined) {
+    return num1 + num2;
+  } else {
+    return cb(num1 + num2);
+  }
+}
+
+// console.log(add(9, 40, Math.sqrt));// 7
+// console.log(add(9, 40));// 49
+
+function isPrime(num) {
+  if(num <= 1) return false;
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+}
+
+// console.log(isPrime(12));
+
+
+// that array methods take in a callback
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+//filter an array of numbers to only prime
+let primes = numbers.filter(isPrime);
+
+// console.log(primes);
+
+let timesTwo = num => num * 2;
+
+console.log(numbers.map(timesTwo));
+// [
+//   2,  4,  6, 8,
+//  10, 12, 14
+// ]
