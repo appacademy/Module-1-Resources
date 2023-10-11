d824f164326e458f9d787e3d2de1e467254ca07a
brandon <brandonlaursen95@yahoo.com>
2023-10-11 11:13:13 -0400

    cb notes

---
+++ w2/d3/lecture.js
@@ -0,0 +1,44 @@
+/*
+
+  Callbacks!
+
+  We previously learned that functions are first class objects!
+    Three reason why!
+      * 1. Be stored in a variable:
+        ** Function Expression
+      * 2. Passed as an argument to a function:
+        ** Callback Functions
+      * 3. Returned from a function:
+        ** Closures/Recursion
+
+  What is a callback function?
+    * A callback function is a function passed into another function as an argument
+    * Which is then invoked inside the outer function to complete some kind of routine or action
+
+  What is a higher order function?
+    * Function that receives or returns a function
+
+  When passing a anonymous callback function, use a arrow function
+    * Much cleaner and shorter
+    [1,2,3].map(num => num * 10);// [10,20,30];
+    * vs
+    [1,2,3].map(function(num){
+      return num * 10;
+    });
+    * Its also MDN's recommended way
+
+  Callback vs Helper functions
+  * All callbacks are helper functions
+  * But not all helper functions are callbacks
+    * Callbacks must be explicitly passed as an argument to a function
+
+
+  ? Call Stack
+  * When a function is called, it is pushed on to the stack
+  * When a function is returned, it is popped off the stack
+  * Whatever function is currently at the top of the stack, is js focus of attention
+  * stack - first in last out
+
+
+
+*/
