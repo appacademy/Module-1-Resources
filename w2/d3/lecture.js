c0862a6b9f08b91284b64f15bd7704563c42b6ec
brandon <brandonlaursen95@yahoo.com>
2023-10-11 11:46:23 -0400

    doubledNumbers

--- w2/d3/lecture.js
+++ w2/d3/lecture.js
@@ -32,13 +32,81 @@
   * But not all helper functions are callbacks
     * Callbacks must be explicitly passed as an argument to a function
 
-
   ? Call Stack
   * When a function is called, it is pushed on to the stack
   * When a function is returned, it is popped off the stack
   * Whatever function is currently at the top of the stack, is js focus of attention
   * stack - first in last out
 
+*/
+let callback = () => console.log('hello from callback function');
 
+// higher order function
+function higherOrder(param) {
+  console.log('hello from higher order function');
+  // console.log(param);// [Function: callback]
+  // console.log(param())
+  param();// hello from callback function
+};
+
+
+// let name = 'brandon';
+// let age = 27
+// console.log(higherOrder(age));
+
+
+// console.log(callback);// [Function: callback]
+
+// higherOrder(callback);
+
+
+
+/*
+
+  Different ways to pass function as callbacks
+
+  1. Anonymous Implicit Callback
+    * Implicit
+    * Preferred way
+    (name) => name.includes('o')
+  2. Anonymous Explicit Callback
+    * Explicit
+    * Uses return keyword + {}
+      (name) => {
+        return name.includes('o');
+      }
+  3. Anonymous Callback Function Keyword
+    * Uses function keyword
+    function(name) {
+      return name.includes('o');
+    }
 
 */
+
+
+
+// higher order function
+function doubleNumbers(numbers, callback) {
+  // console.log(numbers, callback);//[ 1, 2, 3, 4, 5 ] [Function: double]
+  // console.log(callback());NaN
+  // console.log(callback(23));// 46
+  let doubled = [];
+  for(let i = 0 ;i < numbers.length; i++){
+    let number = numbers[i];
+    // console.log(number);
+    // console.log(callback(number));
+    console.log('before passing num to callback', number)
+    let doubledNum = callback(number);
+    console.log('after passing num to callback', doubledNum)
+    doubled.push(doubledNum);
+  };
+
+  return doubled;
+};
+
+
+let array = [1,2,3,4,5];
+let double = (number) => number * 2;
+
+
+console.log(doubleNumbers(array, double));
