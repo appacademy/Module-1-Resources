00183d67845ded56c1b1bfd1d8f0ed31d28d17b1
brandon <brandonlaursen95@yahoo.com>
2023-10-17 15:51:31 -0400

    end of lecture

--- w3/d2/sumArr.js
+++ w3/d2/sumArr.js
@@ -11,8 +11,28 @@ sumArray([1, 2, 3, 4, 5]); // 15
 ***********************************************************************/
 
 
+function sumArray(arr) {
 
+  if(arr.length === 0) return 0;
+
+  // let [first, ...rest] = arr;
+
+  let shifted = arr.shift();
+
+  return shifted + sumArray(arr);
+}
 
 
 // console.log(sumArray([0, 1, -3])); //  -2
+/*
+Bottom Of Stack
+sumArray([0, 1, -3]) => 0 + sumArray([1, -3])
+sumArray([1, -3])    => 1 + sumArray([-3])
+sumArray([-3])       => -3 + sumArray([]);
+sumArray([])         => return 0 - Top Of Stack
+sumArray([-3])       => -3;
+sumArray([1, -3])    => -2;
+sumArray([0, 1, -3]) => -2;
+Bottom Of Stack
+*/
 // console.log(sumArray([1, 2, 3, 4, 5])); // 15
