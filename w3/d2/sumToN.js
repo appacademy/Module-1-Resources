8657c304af69baecb339a2f1a888294ac46b0abd
brandon <brandonlaursen95@yahoo.com>
2023-10-17 15:41:50 -0400

    sumToN

--- w3/d2/sumToN.js
+++ w3/d2/sumToN.js
@@ -17,17 +17,35 @@ sumToN(-8)  // returns null
   null + 1 + 2 + 3 = 6
 */
 
-console.log(null + 0)
+// console.log(null + 0)
 function sumToN(n) {
 
   if(n < 0) return null;
-  // if(n === 0) return 1;
+  // if(n === 1) return 1;
 
   return sumToN(n - 1) + n;
 }
 
-//sumToN(1);
+let res = sumToN(3)
+let res1 = sumToN(2)
+let res2 = sumToN(1)
+let res3 = sumToN(0);
+// console.log(res + res1 + res2 + res3);
+// console.log(sumToN(3));
 
-console.log(sumToN(1))  // returns 1
-console.log(sumToN(9))  // returns 45
-console.log(sumToN(-8))  // returns null
+/*
+Bottom Of Stack
+sumToN(3) => sumToN(2) + 3;
+sumToN(2) => sumToN(1) + 2;
+sumToN(1) => sumToN(0) + 1;
+sumToN(0) => sumToN(-1) + 0;
+sumToN(-1) => null - Top Of Stack
+sumToN(0) => 0
+sumToN(1) => 1
+sumToN(2) => 3
+sumToN(3) => 6
+Bottom Of Stack
+*/
+// console.log(sumToN(1))  // returns 1
+// console.log(sumToN(9))  // returns 45
+// console.log(sumToN(-8))  // returns null
