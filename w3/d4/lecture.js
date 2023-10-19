43ad40cb071442708594227e96bd56a6b4dd2c8f
brandon <brandonlaursen95@yahoo.com>
2023-10-19 11:30:08 -0400

    notes

--- w3/d4/lecture.js
+++ w3/d4/lecture.js
@@ -45,4 +45,45 @@ function synchronous() {
       * users have short attention spans
     * more dynamic websites, allowing people to do more things at once
     * the environment in which we run our application is full of uncertainties
+
+
+   What is the Event Loop?
+    * Consists of the call stack and the message queue
+    * Checking if the call stack is empty, if it is we can move task from the message queue to the call stack
+
+  Call stack - the track the current task in progress
+    * Synchronous task
+    * Last in first out - first in last out
+       * ex: stack of plates
+    * Add to stack - pushing
+    * Removing from the stack - popping
+    * Stack structure
+
+  Message Queue - keep tracks of task that cannot be executed at the moment
+    * Asynchronous task
+    * First in first out - last in last out
+      * ex: standing in line
+    * add to back - enqueue
+    * remove from the front - dequeue
+    * queue structure
+
+    * Task can only be performed one at a time because?
+    * JS is a single threaded language!
 */
+
+
+// async code
+
+console.log('first');
+
+setTimeout(() => {
+  console.log('second');
+}, 5000);
+
+setTimeout(() => {
+  console.log('third')
+},0 );
+
+console.log('fourth');
+
+// first -> fourth -> third -> second
