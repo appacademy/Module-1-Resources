4ce1be149bf927e4885edc6aa96a7012266625d2
brandon <brandonlaursen95@yahoo.com>
2023-10-31 16:29:28 -0400

    conditonals notes

---
+++ w1/d2/conditionals.js
@@ -0,0 +1,29 @@
+/*
+
+
+ Conditionals
+ * let us control what we decide to do, creates a decision making tree for our code
+
+  Talk about truthy and falsy values
+  * https://developer.mozilla.org/en-US/docs/Glossary/Truthy#:~:text=In%20JavaScript%2C%20a%20truthy%20value,type%20coercion%20in%20Boolean%20contexts.
+
+  * Conditional statements will act as control for the flow of our apps and functions.
+  * The basic structure of a conditional statement looks like:
+      if (condition or comparison){
+        <run this code>
+      } else if (other condition or comparison) {
+        <run this code>
+      } else {
+        <do this code if none of the conditions are met>
+      }
+
+  Mutually exclusive condition
+   * A general rule of thumb is that if you are working with a condition that is mutually exclusive, meaning if one condition is true the other condition must be false, then you should  use an if/else statement.
+   * You can also think of mutually exclusivity like a coin flip - it can be either heads or tails but not both.
+
+  Conditionals cans be used to control the flow of our program
+    * by providing conditions, and commands on what to do if conditions are met or not
+    * there are many types of conditions we can check
+
+*/
+
