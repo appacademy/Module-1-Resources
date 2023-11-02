f6f317345af6a294df74e74f3a1d7d9592969597
brandon <brandonlaursen95@yahoo.com>
2023-11-02 12:03:43 -0400

    nested loops

---
+++ w1/d4/lecture.js
@@ -0,0 +1,38 @@
+/*
+
+ Nested Loops
+  * Sometimes a single loop is not enough to access a nested array
+  In that case, you'll write a loop within a loop
+
+*/
+
+for (let i = 0; i < 4; i++) {
+  // console.log("i:", i); // outer loop controlled by i
+
+  for (let j = 1; j < 3; j++) {
+    // console.log("j: ", j); // inner controlled by j
+  }
+}
+
+// outer loop pauses until the inner loop can complete
+// since out inner loop is defined within the bound of our outer loop
+// the full cycle of each our inner loop is prat of each iteration of our outer loop
+
+
+let matrix = [
+  ["a", "b", "c"],
+  ["d", "e", "f"],
+  ["g", "h", "i"],
+];
+
+
+// console.log(matrix[0]);// [ 'a', 'b', 'c' ]
+// console.log(matrix[1]);// [ 'd', 'e', 'f' ]
+// console.log(matrix[2]);// [ 'g', 'h', 'i' ]
+// console.log(matrix[0][0])// a
+// console.log(matrix[0][1])// b
+// console.log(matrix[0][2])// c
+// console.log(matrix[1][0])// d
+// let i = 0;
+// let j = 0;
+// console.log(matrix[i][j]);// a
