3b65fc5b9ce42b11d674772f878166182b6f3e49
brandon <brandonlaursen95@yahoo.com>
2023-11-06 14:17:19 -0500

    lunch

--- w2/d1/fat-arrow-notes.js
+++ w2/d1/fat-arrow-notes.js
@@ -5,6 +5,7 @@
     * More convenient way to pass callback functions(We will learn about on Wednesday)
     * Arrow functions are the preferred syntax when using an anonymous function as a callback.
       ** [1,2,3].map(num => num * 2);// [2,4,6]
+
          [1,2,3].map(function(num){
               return num * 2
            });
---
+++ w2/d1/lecture.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+let sayHello = name => "hello " + name;// line 2
+// console.log(sayHello('brandon'))
+
+
+let math = (num1, num2) => num1 + num2;
+// console.log(math(10,20))
+
+
+// let obj = {};
+
+
+let returnObj = (name) => ({name});
+
+
+// console.log(returnObj('maica'));// { name: 'maica' }
