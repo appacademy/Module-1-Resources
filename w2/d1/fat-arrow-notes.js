db4a4f6c0aee28ea9aab486d7bfd511ebd3ef046
brandon <brandonlaursen95@yahoo.com>
2023-11-06 12:40:01 -0500

    lecture notes for objects

---
+++ w2/d1/fat-arrow-notes.js
@@ -0,0 +1,88 @@
+/*
+
+ What are Fat arrow functions?
+    * Arrow functions, a.k.a. Fat Arrows (=>), are a more concise way of declaring functions.
+    * More convenient way to pass callback functions(We will learn about on Wednesday)
+    * Arrow functions are the preferred syntax when using an anonymous function as a callback.
+      ** [1,2,3].map(num => num * 2);// [2,4,6]
+         [1,2,3].map(function(num){
+              return num * 2
+           });
+    * Differences
+      ! This - context - we learn in week 4
+      * functionally work the same
+
+*/
+
+// Function expression
+// * store an anon function in a variable
+
+let hello = function (name) {
+  return "Hello " + name;
+};
+
+// console.log(hello('brandon'));
+
+/*
+ * Multiple statement arrow function
+    (parameters, go, here) => {
+      statement1;
+      statement2;
+      return <a value>;
+    }
+*/
+
+// Fat arrow function
+let arrowHello = (name) => {
+  let res = "Hello " + name; // statement 1
+  return res; // statement 2
+};
+
+// console.log(arrowHello('Maica'))
+
+// Implicit return
+/*
+  Single expression arrow functions
+  * Can do Implicit returns
+  * Must be a function with a single expression block
+    * argument => expression;
+    * Expression: a line of code that returns a value
+  * Omit the curlys and the return keyword
+*/
+
+let implicitHello = (name) => "Hello " + name; // statement 1
+// console.log(implicitHello('Charles'));
+
+let addition = (num1, num2) => num1 + num2;
+
+// console.log(addition(10, 20));//
+
+// Return an object using an implicit return
+// let obj = { name: 'brandon'};
+
+let returnObj = (name) => ({ firstName: name });
+// console.log(returnObj('Trevor'));// { firstName: 'Trevor' }
+function returnObj2(name) {
+  return { firstName: name };
+}
+
+
+
+
+function example(){
+
+}
+
+
+let example2 = function(){
+
+};
+
+let example3 = () => {};
+
+
+
+
+// example();
+
+// let example4 = () =>
---
+++ w2/d1/pojo-iteration-notes.js
@@ -0,0 +1,141 @@
+/*
+
+Iterating through objects
+  * Objects store unordered key:value pairs
+  * Cannot rely on indices to access values
+  * We will have to use different ways to access and keys and values in an object
+
+  * Well be covering
+    ** for in
+    ** Object.values
+    ** Object.keys
+    ** Object.entries
+
+  ! If we can can gain access to the key; we can gain access to the value
+
+*/
+
+// * a people object with nested person objects
+let people = {
+  person1: {
+    name: "brandon",
+  },
+  person2: {
+    name: "maica",
+  },
+  person3: {
+    name: "charles",
+  },
+  person4: {
+    name: "trevor",
+  },
+};
+
+// access a person in order to do so; we need the key
+// console.log(people);
+// console.log(typeof [1,2,3]);// object
+// console.log(Array.isArray([1,2,3]));
+// console.log(people.person1);// { name: 'brandon' }
+// console.log(people.person2);// { name: 'maica' }
+// console.log(people.person3);// { name: 'charles' }
+
+// console.log(people.length);// undefined
+// ! cant loop through object
+// for(let i = 0; i < people.length; i++){
+//   console.log('hello');
+// }
+
+/*
+
+  For in loop
+  * Can be used to get all the keys
+  * the variable after the let keyword is bananable
+    ** meaning you can name it anything
+    ** it will be the variable used to represent the key
+  * We can use this key variable to 'key' into the object to get the value
+
+  ! Don't use for in loop with arrays/strings
+  ! Use only on objects
+
+*/
+
+for (let key in people) {
+  // console.log(key);
+  // console.log(people);
+  // let value = people[key];
+
+  // console.log(people. key);// ! will print undefined
+  // console.log("key:", key, "value:", value);
+
+  let personObj = people[key];
+  // console.log(personObj)
+  let name = personObj.name;
+  // console.log(name);
+}
+
+// * we could loop or do this
+// ? which is more dynamic?
+// console.log(people.person1.name);
+// console.log(people.person2.name);
+// console.log(people.person3.name);
+// console.log(people.person4.name);
+
+let brandon = {
+  name: "brandon",
+  age: 27,
+  favoriteBands: ["Joyce Manor", "Neck Deep", "Idles"],
+  location: { city: "Tampa", state: "Florida" },
+  sayHello: function (name) {
+    return "Hello " + name;
+  },
+};
+
+for (let key in brandon) {
+  // console.log(key)
+  let value = brandon[key];
+  // console.log(value);
+}
+
+// for(let key in object){}
+// ! TypeError: brandon is not iterable
+// ! cant use for of on object
+// for(let key of brandon){}
+
+// Object.keys
+// return an array of keys
+
+let keysArray = Object.keys(brandon);
+
+console.log(keysArray);
+// [ 'name', 'age', 'favoriteBands', 'location', 'sayHello' ]
+
+// console.log(brandon[keysArray[0]]);//brandon
+// console.log(brandon[keysArray[1]]);//brandon
+// console.log(brandon[keysArray[2]]);//brandon
+
+for (let i = 0; i < keysArray.length; i++) {
+  let key = keysArray[i];
+  // console.log(key);
+  let value = brandon[key];
+  // console.log(key, value)
+}
+
+// Object.values
+// return an array of values
+
+// let valuesArr = Object.values(brandon);
+// console.log(valuesArr);
+
+// Object.entries
+// return a 2d matrix of sub arrays with the first element in the sub array as the key and the second as the value
+
+let entryArr = Object.entries(brandon);
+
+// console.log(entryArr);
+
+let [key1, value1] = ['name','brandon'];
+
+for (let [key, value] of Object.entries(brandon)) {
+  console.log(key);
+  console.log(value);
+}
