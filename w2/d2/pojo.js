537ce5d35d9a893788e2744fd10e4fa500132801
brandon <brandonlaursen95@yahoo.com>
2023-11-07 11:29:13 -0500

    pojo problem 13

---
+++ w2/d2/pojo.js
@@ -0,0 +1,61 @@
+/***********************************************************************
+Write a function `countScores(people)` that takes in an array of score
+objects (people) as its input. A score object has two key-value pairs:
+the scorer (string) and a point value (number). `countScores(people)` should
+return an object that has key-value pairs listing each person who scored as a key
+and the sum of the total points for the game as their value.
+
+
+Example 1:
+let ppl = [{name: "Anthony", score: 10},
+            {name: "Fred", score : 10},
+            {name: "Anthony", score: -8},
+            {name: "Winnie", score: 12}];
+
+console.log(countScores(ppl)); //=> { Anthony: 2, Fred: 10, Winnie: 12 }
+
+Example 2:
+let peeps = [
+  {name: "Anthony", score: 2},
+  {name: "Winnie", score: 2},
+  {name: "Fred", score: 2},
+  {name: "Winnie", score: 2},
+  {name: "Fred", score: 2},
+  {name: "Anthony", score: 2},
+  {name: "Winnie", score: 2}
+];
+console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
+***********************************************************************/
+
+function countScores(people) {
+
+  let result = {};
+
+  //for(let i = 0 ; i < people.length; i++) {
+  // let personObj = people[i];
+  // }
+
+  for(let personObj of people) {
+
+    let score = personObj.score;
+    let name = personObj.name;
+
+    if(result[name] === undefined) {
+      result[name] = score;
+    } else {
+      result[name] += score;
+    }
+    console.log(result);
+  };
+
+  return result;
+}
+
+
+let ppl = [{name: "Anthony", score: 10},
+            {name: "Fred", score : 10},
+            {name: "Anthony", score: -8},
+            {name: "Winnie", score: 12}];
+
+console.log(countScores(ppl)); //=> { Anthony: 2, Fred: 10, Winnie: 12 }
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
