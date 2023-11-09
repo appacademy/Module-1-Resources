dbf9698678a2ef65e457e94480201365e8524416
brandon <brandonlaursen95@yahoo.com>
2023-11-09 12:57:14 -0500

    more closure notes

--- w2/d4/closures.js
+++ w2/d4/closures.js
@@ -1,3 +1,32 @@
+
+/*
+
+  Closures
+
+  What is a closure?
+    * A closure is the combination of a function and the lexical environment within which that function was declared.
+    * A closure is when an inner function uses, or changes, variables in an outer function
+
+  Basic closure rules
+    * Closures have access to any variables within its own, as well as any outer function's, scope when they are declared.
+    * A closure will keep reference to all the variables when it was defined even if the outer function has returned.
+
+  Advantages of closures
+    * private state
+      ** data encapsulation
+      ** security
+    * currying
+
+  Disadvantages to closures
+    * Memory intensive - variables are not garbage collected because they are stored in the lexical environment
+
+  Lexical environment
+    * Anytime a function is defined a lexical environment is created
+    * Lexical environments consist of all the variable defined and where they were defined
+
+
+*/
+
 // Example 1: We can access the outer functions variables
 // * This example is showing how were able to access the order variable from the inner oven function
 function pizzaMaker(food) {
