855e969ae4b57c11e1ac06106725be76ad0acd8c
brandon <brandonlaursen95@yahoo.com>
2023-11-16 13:36:21 -0500

    setTimeout

--- w3/d4/setTimeout.js
+++ w3/d4/setTimeout.js
@@ -43,9 +43,9 @@ function time(name){
 // clearTimeout
 // prevent function from ever being called
 
-let timer = setTimeout((name) => {
-  console.log('hello ' + name)
-}, 1000, 'brandon');
+// let timer = setTimeout((name) => {
+//   console.log('hello ' + name)
+// }, 1000, 'brandon');
 
 
 // console.log(timer);
@@ -53,3 +53,79 @@ let timer = setTimeout((name) => {
 // if(isMember){
 //   clearTimeout(timer);
 // }
+
+// delay = 420000
+// setInterval(() => {
+//   delay--;
+//   console.log(delay)
+// }, 1000)
+
+
+
+// fib
+function somethingSlow(n) {
+  if (n === 1 || n === 2) return 1;
+
+  return somethingSlow(n - 1) + somethingSlow(n - 2);
+}
+
+function foo() {
+  console.log("food");
+}
+
+function bar() {
+  console.log("bark");
+  baz();
+}
+
+function baz() {
+  console.log("bazaar");
+}
+
+// setTimeout(foo, 1500);
+// setTimeout(bar, 1000);
+// * tree recursion
+// console.log(somethingSlow(44));
+// predict what would we be the output?<--
+
+
+
+
+
+
+
+
+
+// function delayedPrinter(delaysArr){
+//   debugger;
+//   if(delaysArr.length === 0) return;
+
+//   const currentDelay = delaysArr.shift();
+//   console.log(currentDelay);
+//   debugger;
+
+//   setTimeout(delayedPrinter, currentDelay, delaysArr)
+// };
+
+
+
+// delayedPrinter(delaysArr);
+
+const delaysArr = [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100];
+
+
+
+function delayedPrinter(delaysArr) {
+
+  let totalDelay = 0;
+  delaysArr.forEach((delay) => {
+    totalDelay += delay;
+    setTimeout(() => {
+      console.log(`waiting for ${delay}ms`)
+    }, totalDelay);
+
+  })
+}
+
+
+delayedPrinter(delaysArr);
