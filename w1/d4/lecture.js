ce76b17a0ec1e91269c3d79de072492f165d3316
brandon <brandonlaursen95@yahoo.com>
2023-11-30 12:01:27 -0500

    matrix

--- w1/d4/lecture.js
+++ w1/d4/lecture.js
@@ -81,4 +81,4 @@ function snakeToCamel(string){
 // console.log(snakeToCamel('snakes_go_hiss')); // 'SnakesGoHiss'
 // console.log(snakeToCamel('say_hello_world')); // 'SayHelloWorld'
 // console.log(snakeToCamel('app_academy_is_cool')); // 'AppAcademyIsCool'
-console.log(snakeToCamel('APp_ACADEMY_iS_cOol')); // 'AppAcademyIsCool'
+// console.log(snakeToCamel('APp_ACADEMY_iS_cOol')); // 'AppAcademyIsCool'
