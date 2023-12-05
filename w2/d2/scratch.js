b1cfbaa7ea22149b943d0d95e9a7de87dc7b9ab1
brandon <brandonlaursen95@yahoo.com>
2023-12-05 12:56:00 -0500

    spread with arrays

--- w2/d2/scratch.js
+++ w2/d2/scratch.js
@@ -7,17 +7,17 @@
 
 
 
-function sentenceMaker(...restOfWords){
-  console.log(restOfWords);
+// function sentenceMaker(...restOfWords){
+//   console.log(restOfWords);
 
-  // return word1 + ' '+ word2;
-  let sentence = '';
-  for(let word of restOfWords) {
-    sentence += ' ' + word;
-  };
+//   // return word1 + ' '+ word2;
+//   let sentence = '';
+//   for(let word of restOfWords) {
+//     sentence += ' ' + word;
+//   };
 
-  return sentence;
-}
+//   return sentence;
+// }
 
 
 // console.log(sentenceMaker('hello', 'world'));
@@ -32,12 +32,41 @@ function sentenceMaker(...restOfWords){
 
 //num1 = 10
 //restOfNumbers = [2, 3]
-function product(num1, ...restOfNumbers) {
-  // console.log(num1,restOfNumbers)
-  // return num1 * num2;
-  return restOfNumbers.reduce((acc, num) => acc *= num ,num1)
-}
+// function product(num1, ...restOfNumbers) {
+//   // console.log(num1,restOfNumbers)
+//   // return num1 * num2;
+//   return restOfNumbers.reduce((acc, num) => acc *= num ,num1)
+// }
 
 
-console.log(product(10, 10));//100
-console.log(product(10, 2, 3));//
+// console.log(product(10, 10));//100
+// console.log(product(10, 2, 3));//
+
+
+
+
+
+
+let arr1 = [1,2,3];
+let arr2 = [4,5,6];
+let arr3 = [arr1, arr2, 7,8,9];
+// console.log(arr3);// [ [ 1, 2, 3 ], [ 4, 5, 6 ], 7, 8, 9 ]
+
+let arr4 = [0, ...arr1, 'middle',...arr2, 7,8,9, ...arr1.reverse()];
+// console.log(arr4);
+// [
+  // 1, 2, 3, 4, 5,
+  // 6, 7, 8, 9
+// ]
+//[ 1, 2, 3 , 4, 5, 6 , 7, 8, 9 ]
+
+// use spread to make a copy
+let copy = arr4.slice();
+console.log(copy);
+copy[0] = true;
+
+// console.log(arr4);
+// console.log(copy);
+
+// let copy1 = [...arr4]
+// console.log(copy1);
