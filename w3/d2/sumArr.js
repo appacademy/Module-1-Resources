3d43f02a5b05faf972f160aa6a61c66e3f7e217a
brandon <brandonlaursen95@yahoo.com>
2023-12-12 15:39:09 -0500

    notes

--- w3/d2/sumArr.js
+++ w3/d2/sumArr.js
@@ -10,25 +10,36 @@ sumArray([0, 1, -3]); //  -2
 sumArray([1, 2, 3, 4, 5]); // 15
 ***********************************************************************/
 
-function sumArray(arr) {
-  debugger;
+
+
+function myFunc(num = 23){
+  console.log(num)
+}
+
+// myFunc(10);
+// myFunc();
+
+// ! DEFAULT PARAM <---
+function sumArray(arr, sum = 0) {
+
   // base case
-  if(!arr.length) return 0;
+  if(!arr.length) return sum;
 
   // recursive step
-  let shifted = arr.shift();
-  // console.log(shifted);
+  // let shifted = arr.shift();
+  let popped = arr.pop();
+  sum += popped;
+  // let [first, ...rest] = arr;
+  // 1 [2, 3]
+  // console.log(first, rest);
+  // let first = arr[0]
 
-  debugger;
   // recursive call
-  return sumArray(arr) + shifted;
+  return sumArray(arr, sum);
 
 };
 console.log(sumArray([1, 2, 3])); //  6
-console.log(undefined === 0)
 
-console.log(true)
-console.log(0)
 /*
 
 
