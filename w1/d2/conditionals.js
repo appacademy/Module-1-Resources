71576a7c74b2e1c3d27b54f4383a1e290a76f81c
brandon <brandonlaursen95@yahoo.com>
2024-01-09 17:34:34 -0500

    conditionals

---
+++ w1/d2/conditionals.js
@@ -0,0 +1,79 @@
+/*
+
+ Conditionals
+ * let us control what we decide to do, creates a decision making tree for our code
+
+  Talk about truthy and falsy values
+  * https://developer.mozilla.org/en-US/docs/Glossary/Truthy#:~:text=In%20JavaScript%2C%20a%20truthy%20value,type%20coercion%20in%20Boolean%20contexts.
+
+  * Conditional statements will act as control for the flow of our apps and functions.
+  * The basic structure of a conditional statement looks like:
+      if (condition or comparison){
+        <run this code>
+      } else if (other condition or comparison) {
+        <run this code>
+      } else {
+        <do this code if none of the conditions are met>
+      }
+
+  Mutually exclusive condition
+   * A general rule of thumb is that if you are working with a condition that is mutually exclusive, meaning if one condition is true the other condition must be false, then you should  use an if/else statement.
+   * You can also think of mutually exclusivity like a coin flip - it can be either heads or tails but not both.
+
+  Conditionals cans be used to control the flow of our program
+    * by providing conditions, and commands on what to do if conditions are met or not
+    * there are many types of conditions we can check
+
+  */
+
+
+  let randomNumber = Math.random();
+  // console.log(randomNumber);
+
+  //mutually exclusive condition
+  // console.log(randomNumber);
+  // if(randomNumber > 0.5) {
+  //   console.log('heads')
+  // } else {
+  //   console.log('tails')
+  // }
+
+//non mutually exclusive condition
+// if(randomNumber > 0.5) {
+//   console.log('heads')
+// }
+// if (randomNumber > 0.1) {
+//   console.log('tails')
+// }
+
+
+if(10 < 20) {
+  // console.log('10 is less than 20')
+}
+
+// 4
+// if('cats'.length) {
+//   console.log('this string has length')
+// }
+// // <reference> => []
+// if([]) {
+//   console.log('truthy')
+// }
+
+// if('') {
+//   console.log('this is falsy')
+// }
+
+
+function greaterThanFive(num) {
+
+  if(num > 5) {
+    return `${num} is greater than 5`
+  }
+    return `${num} is not greater than 5`
+  
+}
+
+
+console.log(greaterThanFive(1));// 1 is not greater than 5
+console.log(greaterThanFive(10));// 10 is greater than 5
