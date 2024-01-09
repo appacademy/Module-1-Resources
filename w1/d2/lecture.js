234a1f32298400a7e1ce629f3f9c4ae8e0603d4c
brandon <brandonlaursen95@yahoo.com>
2024-01-09 12:00:02 -0500

    notes

--- w1/d2/lecture.js
+++ w1/d2/lecture.js
@@ -1,1 +1,75 @@
-console.log('hello world');
+
+/*
+
+  Intro to JavaScript
+
+  What is Javascript?
+    * JavaScript is a programming language that was originally developed to increase user interactivity with websites, and has grown to become a useful programming language in the backend of applications as well. JavaScript is now one of the most popular and widely used programming languages for building web applications today.
+
+  Why is javascript so useful?
+    * Perform asynchronous task
+    * Render dynamic content
+    * Enables more responsive, performant websites
+    * Integrates seamlessly with two core languages of web development, HTML and CSS
+
+   * We want to learn JavaScript!
+
+  What is learning?
+    * 1. Learning new skills and being able to apply them in the future
+    * 2. Retaining useful information
+
+  Active vs Passive Learning
+    * Active learning  - lets you engage, get hands on!
+    * Passive learning -  watching not applying
+
+  We will be doing both!
+
+  * Passive followed by active learning!
+  * Lecture followed by self practice
+
+  Desirable difficulty
+    * it's challenging, but not discouraging
+    * Possible given proper skill level
+    * Hard but not too hard
+    * But also not easy
+    * Find a Happy Medium
+
+  Spaced Repetition
+    * Repeating concepts with time between
+    * Rest your brain
+    * Pomodoro Method
+
+  What is debugging?
+    * Figuring out what is wrong with the code
+    * Identifying and solving an error
+
+  How do we debug?
+    * console.log() - show them on mdn
+    * if you want to see something on the screen use a console.log
+    * debugger
+    * go line by line
+    * developer tools
+    * error messages
+    * trial and error
+
+  How to style/format?
+    * Style Guide
+    * https://developer.mozilla.org/en-US/docs/MDN/Writing_guidelines/Writing_style_guide/Code_style_guide/JavaScript
+
+  Whats the deal with semicolons?
+    * JS Automatic semicolon insertion
+    * semicolons Js doesn't strictly need semi colons because its smart, it uses something called automatic semicolon insertion, But it is best practice to use them for finishing statements
+    * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#automatic_semicolon_insertion
+
+
+  * Comments
+  Why do we use comments?
+    * explain what your code is doing
+    * creating a plan using polya's framework!
+    * notes
+
+  * Console.logs
+  When do we use a console.log?
+    * When we want to see something in the terminal
+
+*/
