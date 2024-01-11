061b345a27932ce994b3d0ad33e82417b6a6e588
brandon <brandonlaursen95@yahoo.com>
2024-01-11 12:46:41 -0500

    notes

---
+++ w1/d4/nested-loop.js
@@ -0,0 +1,59 @@
+/*
+
+
+  Nested Loops
+  * Sometimes a single loop is not enough to access a nested array
+  In that case, you'll write a loop within a loop
+  example
+
+    outer loop pauses until inner loop can complete
+    Since our inner loop is defined within the bounds of our outer
+    loop, the full cycle of our inner loop is part of each individual iteration
+    of our outer loop. Let's explore how we can use this a bit more practically
+*/
+
+
+for(let i = 0; i < 3; i++) {
+  // console.log('i:',i);// outer loop controlled by i
+
+  for(let j = 0; j < 2; j++) {
+    // console.log('j: ',j);// inner loop controlled by j
+  }
+}
+
+// 2 array or matrix
+let matrix = [
+  ["a", "b", "c"],
+  ["d", "e", "f"],
+  ["g", "h", "i"],
+];
+
+// console.log(matrix.length);// 3
+// console.log(matrix[0]);// [ 'a', 'b', 'c' ]
+// console.log(matrix[1]);// [ 'd', 'e', 'f' ]
+// console.log(matrix[2]);// [ 'g', 'h', 'i' ]
+// console.log(matrix[0][0]);// a
+// console.log(matrix[0][1]);// b
+// console.log(matrix[0][2]);// c
+// console.log(matrix[1][0]);// d
+// console.log(matrix[1][1]);// e
+// let subArr = matrix[2];
+// console.log(matrix[2][0]);// g
+// console.log(subArr[0]);// g
+// let i = 0;
+// console.log(matrix[i]);//[ 'a', 'b', 'c' ]
+// let j = 0;
+// console.log(matrix[i][j]);// a
+// console.log(matrix[0][0]);// a
+
+
+let matrix1 = [
+  ["a", "b", "c"],
+  ["d", "e", "f"],
+  ["g", "h", "i"],
+];
+
+
+for(let i = 0; i < matrix1.length; i++){
+  console.log(i);
+}
