fff508e025a13a9a4c6922c65659a750ebcbad48
brandon <brandonlaursen95@yahoo.com>
2024-01-18 13:47:07 -0500

    nested array of objects

---
+++ w2/d3/forOf-vs-forIn.js
@@ -0,0 +1,42 @@
+
+
+//for of
+
+let numbers = [1,2,3];
+
+for(let number of numbers){
+  console.log(number);
+}
+
+
+let obj = {name:'brandon', age: 28};
+//! cant use for of on object
+// for(let ele of obj) {
+//   console.log(ele);
+// }
+// TypeError: obj is not iterable
+
+
+// for in
+// ! on objects!
+for(let key in obj) {
+  console.log(key);// name | age
+}
+let array1 = ['HELLO', 'WORLD', true];
+
+let array2 = {
+  0:'HELLO',
+  1:'WORLD',
+  2: true
+}
+
+console.log(array2[0]);//HELLO
+console.log(array1[0]);//HELLO
+
+// for(let index in numbers) {
+//   console.log(index, typeof index);
+// // }
+
+
+// FOR OF ONLY ON ARRAYS
+// FOR IN ONLY OBJECTS
