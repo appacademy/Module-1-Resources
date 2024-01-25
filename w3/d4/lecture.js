e772ddbac4c28bcbbda9d40def6da0b97193dd85
brandon <brandonlaursen95@yahoo.com>
2024-01-25 11:44:46 -0500

    async code

---
+++ w3/d4/lecture.js
@@ -0,0 +1,101 @@
+
+/*
+
+  What does it mean for javascript to be a single threaded language?
+    * Any instance during the program, only one task is being executed!
+    * The current task will run to full completion, before the next task is handled
+
+
+  No. NodeJs is not single threaded.
+  The NodeJs event loop operates on a single thread yes,
+  but the async blocking operations are delegated to separate
+  worker threads.
+
+
+  What is multithreaded?
+    * multiple commands can be processed at the same time
+    * python
+
+  Synchronous vs Asynchronous code
+
+  What is Synchronous code?
+    * Runs line by line
+    * Commands executed one after another, only one command a time
+    * Code that has inherit order
+    * Order of execution is guaranteed
+
+*/
+
+// Synchronous code
+// * We can predict the order of this code!
+
+function synchronous() {
+  console.log("two");
+}
+
+// console.log("one");
+// synchronous();
+// console.log("three");
+
+// one => two => three
+
+/*
+  Asynchronous code?
+    * no guaranteed order
+    * does not block the execution of the call stack
+
+  Why do we need async code?
+    * user clicks a button,
+    * have to send a api call
+    * perform two things at once,
+    * interact with the site we cant predict
+      * users have short attention spans
+    * more dynamic websites, allowing people to do more things at once
+    * the environment in which we run our application is full of uncertainties
+
+  What is the Event Loop?
+    * Consists of the call stack and the message queue
+    * Checking if the call stack is empty, if it is we can move task from the message queue to the call stack
+
+  Call stack - the track the current task in progress
+    * Synchronous task
+    * Last in first out - first in last out
+       * ex: stack of plates
+    * Add to stack - pushing
+    * Removing from the stack - popping
+    * Stack structure
+
+  Message Queue - keep tracks of task that cannot be executed at the moment
+    * Asynchronous task
+    * First in first out - last in last out
+      * ex: standing in line
+    * add to back - enqueue
+    * remove from the front - dequeue
+    * queue structure
+
+    * Task can only be performed one at a time because?
+    * JS is a single threaded language!
+
+    NodeApi/Web api -  Built using C, interacting with API
+      * DOM
+      * ajax
+      * setTimeout
+*/
+
+
+// console.log("first");
+
+// // This does not block the execution of the call stack
+// // * Because async code is sent to the message queue
+// setTimeout(() => {
+//   console.log("second");
+// }, 0);
+
+// setTimeout(() => {
+//   console.log("third");
+// }, 0);
+
+
+// console.log("fourth");
+// first => fourth => second => third
+// * not what we expect
